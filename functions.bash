76a6124e85c38e6609f4621efdca2c5353f8ef47
Harsh Shandilya <msfjarvis@gmail.com>
2017-01-16 17:56:41 +0530

    Update functions

--- functions.bash
+++ functions.bash
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-source xg.bash
+function add() {
+  repo_name=$(printf '%s\n' "${PWD##*/}")
+  git remote add gerrit ssh://MSF-Jarvis@review.halogenos.org:29418/$repo_name
+  hook
+}
+
+function gpush() {
+  if [[ ! "$(git remote)" == *"gerrit"* ]]; then
+  add
+  fi
+  git push gerrit HEAD:refs/for/XOS-7.0
+}
+
+function gfpush() {
+  if [[ ! "$(git remote)" == *"gerrit"* ]]; then
+  add
+  fi
+  git push gerrit HEAD:refs/heads/XOS-7.0
+}
 
 function cleanapks() {
   apks=$(find . -name *.apk 2>/dev/null)
-  echo "$(echo $apks | wc -l) APKs found"
   for i in $apks; do rm $i; done
 }
 
@@ -13,7 +30,6 @@ function cleanmahshitz() {
   rm -rf app/build
 }
 
-# kanged from transfer.sh, but oh well I did the formatting by hand
 function transfer() {
   if [ $# -eq 0 ]
     then echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"
@@ -34,19 +50,24 @@ function gpick(){
 function jenkinsbuild(){
   if [ $1 ];
   then
-  jenkins_invoke -u $JENKINSUSERNAME -p $JENKINSPASSWD -J $JENKINSURL $1
+  jenkins_invoke -u $JENKINSUSERNAME -p $JENKINSPASSWD -J http://jenkins.msfjarvis.me $1
   else
   echo "Please specify a jenkins job to begin!"
   fi
 }
 
+function hook() {
+  scp -p -P 29418 MSF-Jarvis@review.halogenos.org:hooks/commit-msg .git/hooks/
+}
+
 function twrp() {
+  #echo $@ && return 1
   if  [ "$1" == "install" ]; then
   if [ "$2" != "" ]; then
   adb push $2 /external_sd/
   adb shell twrp install /external_sd/$2
   else
-  echo "install what, you twat?" && return 1
+  echo "install what you twat" && return 1
   fi
   else
   echo "fark orf ya carnt" && return 1
@@ -56,3 +77,36 @@ function twrp() {
 function myeyes(){
   xflux -l 28.6869 -g 77.3525 -r 1 -k 3000
 }
+
+function updatee(){
+  for i in $(ls); do cd $i && git pull origin XOS-7.0 && cd .. ; done
+}
+
+function what() {
+  bash-it help aliases $1 | grep $2 2>/dev/null
+}
+
+function findapks(){
+  find . -name *.apk
+}
+
+function datime(){
+  date '+%A %W %Y %X'
+}
+
+function subsupdate(){
+  cur_dir=$(pwd)
+  cd ~/git-repos/substratum
+  git checkout dev
+  git pull origin dev
+  git checkout self-builds
+  git pull origin dev
+  git push --all
+  cd $cur_dir
+}
+
+function weather(){
+  [ -z "$1" ] && curl 'wttr.in/New%20Delhi' || curl "wttr.in/$1"
+}
+
+alias disp="xrandr --output eDP1 --rotate $1"
