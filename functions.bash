8464980a1995da65eb04157f91296da8b3daf757
Akhil Narang <akhilnarang.1999@gmail.com>
2017-07-02 13:37:58 +0530

    functions: Simplify transfer command

--- functions.bash
+++ functions.bash
@@ -28,12 +28,7 @@ function transfer() {
     cat /tmp/test.md | transfer test.md"
     return 1
   fi
-  tmpfile=$( mktemp -t transferXXX )
-  if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
-  curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile
-  else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile
-  fi; cat $tmpfile
-  rm -f $tmpfile
+  curl --progress-bar --upload-file "$1" "https://transfer.sh/";printf '\n'
 }
 
 function gpick(){
