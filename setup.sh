2d885946126a0b72cc27159b647f035bcce91b39
Harsh Shandilya <harsh@prjkt.io>
2018-04-14 15:19:17 +0530

    setup: Allow for having encrypted gitconfig fragments
    More portable for my lazy ass if I can keep it in the
    git repository.
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- setup.sh
+++ setup.sh
@@ -33,7 +33,11 @@ if [[ "$@" =~ "--install-gitconfig" || "$@" =~ "--all" ]]; then
   mv ~/.gitconfig ~/.gitconfig.old # Failsafe in case we screw up
   cp ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
   for item in $(find gitconfig_fragments -name fragment_*); do
-    cat ${item} >> ~/.gitconfig
+    decrypted=$(echo ${item} | cut -d '.' -f 1)
+    rm -f ${decrypted} 2>/dev/null
+    gpg ${item}
+    [[ ! -f ${decrypted} ]] && return
+    cat ${decrypted} >> ~/.gitconfig
   done
 fi
 
