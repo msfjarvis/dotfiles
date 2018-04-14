4e95fa636b8f09bd2d87370230fb50f50a346c20
Harsh Shandilya <harsh@prjkt.io>
2018-04-14 14:26:36 +0530

    setup: Add support for tdm-scripts
    I don't use all of them just yet so only copy the ones
    I do via a whitelist.
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- setup.sh
+++ setup.sh
@@ -4,12 +4,17 @@ SCRIPT_DIR="$(cd "$( dirname $( readlink -f "${BASH_SOURCE[0]}" ) )" && pwd)"
 source ${SCRIPT_DIR}/common
 
 declare -a SCRIPTS=("build-xos" "build-caesium" "build-twrp" "hastebin")
+declare -a TDM_SCRIPTS=("gerrit-review")
 
 mkdir -p ~/bin/
 
-for SCRIPT in ${SCRIPTS[@]};do
+for SCRIPT in ${SCRIPTS[@]}; do
     rm -rf ~/bin/${SCRIPT}
     ln -s ${SCRIPT_DIR}/${SCRIPT} ~/bin/${SCRIPT}
+done
+
+for ITEM in ${TDM_SCRIPTS[@]}; do
+    cp tdm-scripts/${ITEM} ~/bin/${ITEM}
 done
 
 if [[ ! $(echo $PATH) =~ /home/$(whoami)/bin ]]; then
@@ -27,7 +32,7 @@ if [[ "$@" =~ "--install-gitconfig" || "$@" =~ "--all" ]]; then
   echoText "Setting up gitconfig"
   mv ~/.gitconfig ~/.gitconfig.old # Failsafe in case we screw up
   cp ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
-  for item in $(find gitconfig_fragments -name fragment_*);do
+  for item in $(find gitconfig_fragments -name fragment_*); do
     cat ${item} >> ~/.gitconfig
   done
 fi
