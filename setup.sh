5d13ebabb7ff729e71429e2c42d3c0f6f1a15d12
Harsh Shandilya <harsh@prjkt.io>
2018-09-02 15:51:18 +0530

    setup: Add diff-so-fancy installation
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- setup.sh
+++ setup.sh
@@ -39,6 +39,21 @@ else
     reportWarning "gdrive is already installed!"
 fi
 
+echoText "Installing 'diff-so-fancy'"
+DIFF_SO_FANCY="$(command -v diff-so-fancy)"
+if [[ "${DIFF_SO_FANCY}" == "" ]]; then
+    sudo wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -O /usr/local/bin/diff-so-fancy
+    sudo chmod +x /usr/local/bin/diff-so-fancy
+else
+    INSTALLED_VERSION="$(grep 'my $VERSION = ' /usr/local/bin/diff-so-fancy | cut -d \" -f 2)"
+    LATEST_VERSION="$(get_latest_release so-fancy/diff-so-fancy | sed 's/v//')"
+    if [[ "${INSTALLED_VERSION}" != "${LATEST_VERSION}" ]]; then
+        sudo wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -O /usr/local/bin/diff-so-fancy
+        sudo chmod +x /usr/local/bin/diff-so-fancy
+    fi
+fi
+unset DIFF_SO_FANCY
+
 echoText "Importing GPG keys"
 for KEY in "${GPG_KEYS[@]}"; do
     gpg --import "${SCRIPT_DIR}"/gpg_keys/"${KEY}"
