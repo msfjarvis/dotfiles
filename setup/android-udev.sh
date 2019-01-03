13f40f3425c0a81549ddc257f711b8080719ac0b
Harsh Shandilya <msfjarvis@gmail.com>
2019-01-03 23:27:39 +0530

    setup/android-udev: Shuffle around output silencing
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- setup/android-udev.sh
+++ setup/android-udev.sh
@@ -7,7 +7,7 @@ function setup_android_udev {
     local SCRIPT_DIR; SCRIPT_DIR="$(cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" && pwd)"
     source "${SCRIPT_DIR}"/../common
     echoText "Installing latest Android udev rules"
-    curl "https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules" | sudo tee /etc/udev/rules.d/51-android.rules >/dev/null 2>&1
+    curl "https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules" 2>/dev/null | sudo tee /etc/udev/rules.d/51-android.rules 1>/dev/null
     sudo chmod a+r /etc/udev/rules.d/51-android.rules
     sudo udevadm control --reload-rules
     sudo service udev restart
