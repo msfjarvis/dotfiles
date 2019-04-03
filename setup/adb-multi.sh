cc5d129d3052c8db4882d5c4ac0c6d46277de339
Harsh Shandilya <msfjarvis@gmail.com>
2019-04-03 22:17:55 +0530

    setup/adb-multi: Remove patchwork
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- setup/adb-multi.sh
+++ setup/adb-multi.sh
@@ -7,14 +7,9 @@ source "${SCRIPT_DIR}/common"
 
 function setup_adb {
     echoText "Setting up multi-adb"
-    git -C "${SCRIPT_DIR}/adb-multi" reset --hard
-    if [ -d "${SCRIPT_DIR}/patches/adb-multi" ]; then
-        find "${SCRIPT_DIR}/patches/adb-multi/" -type f -exec git -C "${SCRIPT_DIR}"/adb-multi/ apply {} \;
-    fi
     cp "${SCRIPT_DIR}/config.cfg" "${SCRIPT_DIR}"/adb-multi/config.cfg
     "${SCRIPT_DIR}"/adb-multi/adb-multi generate "${HOME}/bin"
     cp "${SCRIPT_DIR}"/adb-multi/adb-multi ~/bin
-    git -C "${SCRIPT_DIR}/adb-multi" reset --hard
 }
 
 setup_adb
