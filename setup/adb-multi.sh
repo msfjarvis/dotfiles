148a4082b0e81dfaf84e0b7b408612e30177e933
Harsh Shandilya <msfjarvis@gmail.com>
2019-06-05 14:35:14 +0530

    Remove adb-multi submodule
    I don't require a fork anymore.
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- setup/adb-multi.sh
+++ setup/adb-multi.sh
@@ -6,10 +6,16 @@
 source "${SCRIPT_DIR}/common"
 
 function setup_adb {
+    local CLONE_DIR; CLONE_DIR=/tmp/adb-multi;
+    if [ -d "${CLONE_DIR}" ]; then
+        git -C "${CLONE_DIR}" pull origin master
+    else
+        git clone https://github.com/Kreach3r/adb-multi -b master "${CLONE_DIR}"
+    fi
     echoText "Setting up multi-adb"
-    cp "${SCRIPT_DIR}/config.cfg" "${SCRIPT_DIR}"/adb-multi/config.cfg
-    "${SCRIPT_DIR}"/adb-multi/adb-multi generate "${HOME}/bin"
-    cp "${SCRIPT_DIR}"/adb-multi/adb-multi ~/bin
+    cp "${SCRIPT_DIR}/config.cfg" "${CLONE_DIR}"/config.cfg
+    "${CLONE_DIR}"/adb-multi generate "${HOME}/bin"
+    cp "${CLONE_DIR}"/adb-multi ~/bin
 }
 
 setup_adb
