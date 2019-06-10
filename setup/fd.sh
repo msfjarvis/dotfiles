8c68efa03c5aef26ace2ebd99de2eebcdfd7b9ed
Harsh Shandilya <msfjarvis@gmail.com>
2019-06-11 04:53:36 +0530

    setup/fd: Handle upgrades
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- setup/fd.sh
+++ setup/fd.sh
@@ -6,19 +6,31 @@
 source "${SCRIPT_DIR}"/common
 source "${SCRIPT_DIR}"/gitshit
 
-function install_fd {
-    local FD FD_ARTIFACT
+function check_and_install_fd {
+    local FD FD_ARTIFACT LOCAL_FD_VERSION REMOTE_FD_VERSION
     echoText "Checking and installing fd"
     FD="$(command -v fd)"
     if [ "${FD}" == "" ]; then
-        FD_ARTIFACT="fd_.*_amd64.deb"
-        aria2c "$(get_release_assets sharkdp/fd | grep "${FD_ARTIFACT}")" -o fd.deb
-        sudo dpkg -i fd.deb
-        cd "${SCRIPT_DIR}" || return 1
-        rm -rf fd.deb
+        install_fd
     else
-        reportWarning "$(fd --version) is already installed!"
+        LOCAL_FD_VERSION="$(fd --version | awk '{print $2}')"
+        REMOTE_FD_VERSION="$(get_latest_release sharkdp/fd | sed 's/v//')"
+        if [ "${LOCAL_FD_VERSION}" != "${REMOTE_FD_VERSION}" ]; then
+            reportWarning "Outdated version of fd detected, upgrading"
+            install_fd
+        else
+            reportWarning "$(fd --version) is already installed!"
+        fi
     fi
 }
 
-install_fd
+function install_fd {
+    local FD_ARTIFACT; FD_ARTIFACT="fd_.*_amd64.deb"
+    aria2c "$(get_release_assets sharkdp/fd | grep "${FD_ARTIFACT}")" -o fd.deb
+    sudo dpkg -i fd.deb
+    cd "${SCRIPT_DIR}" || return 1
+    rm -rf fd.deb
+
+}
+
+check_and_install_fd
