970db7c103fdb8572ee945bbf50d072956eccdc1
Harsh Shandilya <msfjarvis@gmail.com>
2019-06-13 01:15:13 +0530

    setup/android-udev: Directly use curl to install rules
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- setup/android-udev.sh
+++ setup/android-udev.sh
@@ -7,7 +7,7 @@ source "${SCRIPT_DIR}"/common
 
 function setup_android_udev() {
     echoText "Installing latest Android udev rules"
-    curl "https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules" 2>/dev/null | sudo tee /etc/udev/rules.d/51-android.rules 1>/dev/null
+    sudo curl -s "https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules" -o /etc/udev/rules.d/51-android.rules
     sudo chmod a+r /etc/udev/rules.d/51-android.rules
     sudo udevadm control --reload-rules
     sudo service udev restart
