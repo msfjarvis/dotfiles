msfjarvis/dotfiles

commit f613c8cf2d8c62db942bc32e9bd875ab2dc1ed98

setup: Add ripgrep
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

setup.sh
@@ -19,7 +19,7 @@ mkdir -p ~/bin/
 
 # Install standard packages.
 echoText "Installing necessary packages"
-sudo apt install -y aria2 autoconf automake axel cowsay curl jq libxmu-dev lolcat mosh shellcheck silversearcher-ag wget
+sudo apt install -y aria2 autoconf automake axel cowsay curl jq libxmu-dev lolcat mosh shellcheck wget
 
 # Update all submodules
 git -C "${SCRIPT_DIR}" submodule update --init --recursive

setup/ripgrep.sh
@@ -1,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Copyright (C) Harsh Shandilya <msfjarvis@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-only
+
+source "${SCRIPT_DIR}"/common
+source "${SCRIPT_DIR}"/gitshit
+
+function check_and_install_ripgrep() {
+    local RIPGREP RIPGREP_ARTIFACT LOCAL_RIPGREP_VERSION REMOTE_RIPGREP_VERSION
+    RIPGREP="$(command -v rg)"
+    echoText "Checking and installing ripgrep"
+    if [ -z "${RIPGREP}" ]; then
+        install_ripgrep
+    else
+        LOCAL_RIPGREP_VERSION="$(rg --version | head -n1 | awk '{print $2}')"
+        REMOTE_RIPGREP_VERSION="$(get_latest_release BurntSushi/ripgrep)"
+        if [ "${LOCAL_RIPGREP_VERSION}" != "${REMOTE_RIPGREP_VERSION}" ]; then
+            reportWarning "Outdated version of ripgrep detected, upgrading"
+            install_ripgrep
+        else
+            reportWarning "$(rg --version | head -n1) is already installed!"
+        fi
+    fi
+}
+
+function install_ripgrep() {
+    local RIPGREP_ARTIFACT
+    RIPGREP_ARTIFACT="ripgrep_.*_amd64.deb"
+    cd /tmp || return 1
+    aria2c "$(get_release_assets BurntSushi/ripgrep | grep "${RIPGREP_ARTIFACT}")" -o ripgrep.deb
+    sudo dpkg -i ripgrep.deb
+    rm -rf ripgrep.deb
+    cd "${SCRIPT_DIR}" || return 1
+}
+
+check_and_install_ripgrep

commit 6b303ee8f6cb7fac156faf625ab4cc8b883cd0b8

setup: Add ripgrep to automatic set
Fixes: f613c8cf2d8c ("setup: Add ripgrep")
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

setup.sh
@@ -29,6 +29,7 @@ source "${SCRIPT_DIR}"/setup/fd.sh
 source "${SCRIPT_DIR}"/setup/gdrive.sh
 source "${SCRIPT_DIR}"/setup/gitconfig.sh
 source "${SCRIPT_DIR}"/setup/hub.sh
+source "${SCRIPT_DIR}"/setup/ripgrep.sh
 source "${SCRIPT_DIR}"/setup/xclip.sh
 
 cd "${SCRIPT_DIR}" || exit 1