682eb94bf8f3e040f6828eeceaf278c2ffd22768
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-15 09:30:45 +0530

    bash_completions: Make wgdown completions more useful
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- bash_completions.bash
+++ bash_completions.bash
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-_wg_completions()
+_wgup_completions()
 {
   mapfile -t COMPREPLY < <(compgen -W "$(cd ~/wireguard || exit; fd -tf  \\.conf$ -X echo | sed -e 's/mullvad-//g' -e 's/\.conf//g')" -- "${COMP_WORDS[1]}")
 }
 
-complete -F _wg_completions wgup
-complete -F _wg_completions wgdown
+_wgdown_completions()
+{
+  mapfile -t COMPREPLY < <(compgen -W "$(wg show interfaces | sed 's/mullvad-//g')")
+}
+
+complete -F _wgup_completions wgup
+complete -F _wgdown_completions wgdown
