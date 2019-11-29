52f13956ddb5e66d2bd7677c2befd6f769244dc9
Harsh Shandilya <msfjarvis@gmail.com>
2019-11-29 18:53:25 +0530

    bash_completions: Add sync{up,down} completions
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- bash_completions.bash
+++ bash_completions.bash
@@ -8,5 +8,16 @@ _wgdown_completions() {
   mapfile -t COMPREPLY < <(compgen -W "$(wg show interfaces | sed 's/mullvad-//g')" -- "${COMP_WORDS[1]}")
 }
 
+_syncup_completions() {
+  [ -n "${LOCAL_SITE_MIRROR}" ] || return 0
+  mapfile -t COMPREPLY < <(compgen -W "$(fd -HI --maxdepth=1 . "$LOCAL_SITE_MIRROR"/ -x echo "{/}")" -- "${COMP_WORDS[1]}")
+}
+
+_syncdown_completions() {
+  _syncup_completions
+}
+
 complete -F _wgup_completions wgup
 complete -F _wgdown_completions wgdown
+complete -F _syncup_completions syncup
+complete -F _syncdown_completions syncdown
