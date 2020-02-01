48b15e834da388a0e7dda1f4cd103ae267d6bc10
Harsh Shandilya <msfjarvis@gmail.com>
2020-02-01 13:00:28 +0530

    setup: Also install hyperfine
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- setup.sh
+++ setup.sh
@@ -32,6 +32,7 @@ bash -i "${SCRIPT_DIR}"/setup/ripgrep.sh
 bash -i "${SCRIPT_DIR}"/setup/sharkdp.sh bat
 bash -i "${SCRIPT_DIR}"/setup/sharkdp.sh diskus
 bash -i "${SCRIPT_DIR}"/setup/sharkdp.sh fd
+bash -i "${SCRIPT_DIR}"/setup/sharkdp.sh hyperfine
 bash -i "${SCRIPT_DIR}"/setup/shellcheck.sh
 bash -i "${SCRIPT_DIR}"/setup/shfmt.sh
 bash -i "${SCRIPT_DIR}"/setup/xclip.sh
