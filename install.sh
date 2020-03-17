9d46635836c07a091bc42396317e7763d7d27e15
Harsh Shandilya <me@msfjarvis.dev>
2020-03-17 12:49:04 +0530

    setup/ripgrep: Remove
    Using a Homebrew tap for it
    
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- install.sh
+++ install.sh
@@ -27,7 +27,6 @@ bash -i "${SCRIPT_DIR}"/setup/diff-so-fancy.sh
 bash -i "${SCRIPT_DIR}"/setup/gdrive.sh
 bash -i "${SCRIPT_DIR}"/setup/hub.sh
 bash -i "${SCRIPT_DIR}"/setup/nano.sh
-bash -i "${SCRIPT_DIR}"/setup/ripgrep.sh
 bash -i "${SCRIPT_DIR}"/setup/shellcheck.sh
 bash -i "${SCRIPT_DIR}"/setup/shfmt.sh
 bash -i "${SCRIPT_DIR}"/setup/xclip.sh
