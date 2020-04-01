521fff815272d95503b9f8e97eefc9aa335059a4
Harsh Shandilya <me@msfjarvis.dev>
2020-04-02 00:49:09 +0530

    Add missing files to test matrix and reformat
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- setup/adb-multi.sh
+++ setup/adb-multi.sh
@@ -7,18 +7,18 @@
 source "${SCRIPT_DIR:?}"/setup/common.sh
 
 function setup_adb() {
-    local CLONE_DIR
-    CLONE_DIR="/tmp/adb-multi"
-    if [ -d "${CLONE_DIR}" ]; then
-        git -C "${CLONE_DIR}" pull origin master
-    else
-        git clone https://github.com/Kreach3r/adb-multi -b master "${CLONE_DIR}"
-    fi
-    echoText "Setting up multi-adb"
-    cp "${SCRIPT_DIR}/config.cfg" "${CLONE_DIR}"/config.cfg
-    "${CLONE_DIR}"/adb-multi generate "${HOME}/bin"
-    cp "${CLONE_DIR}"/adb-multi ~/bin
-    cp "${CLONE_DIR}"/config.cfg ~/bin
+  local CLONE_DIR
+  CLONE_DIR="/tmp/adb-multi"
+  if [ -d "${CLONE_DIR}" ]; then
+    git -C "${CLONE_DIR}" pull origin master
+  else
+    git clone https://github.com/Kreach3r/adb-multi -b master "${CLONE_DIR}"
+  fi
+  echoText "Setting up multi-adb"
+  cp "${SCRIPT_DIR}/config.cfg" "${CLONE_DIR}"/config.cfg
+  "${CLONE_DIR}"/adb-multi generate "${HOME}/bin"
+  cp "${CLONE_DIR}"/adb-multi ~/bin
+  cp "${CLONE_DIR}"/config.cfg ~/bin
 }
 
 setup_adb
--- setup/android-udev.sh
+++ setup/android-udev.sh
@@ -7,11 +7,11 @@
 source "${SCRIPT_DIR:?}"/setup/common.sh
 
 function setup_android_udev() {
-    echoText "Installing latest Android udev rules"
-    sudo curl -s --create-dirs -L "https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules" -o /etc/udev/rules.d/51-android.rules
-    sudo chmod a+r /etc/udev/rules.d/51-android.rules
-    sudo udevadm control --reload-rules
-    sudo service udev restart
+  echoText "Installing latest Android udev rules"
+  sudo curl -s --create-dirs -L "https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules" -o /etc/udev/rules.d/51-android.rules
+  sudo chmod a+r /etc/udev/rules.d/51-android.rules
+  sudo udevadm control --reload-rules
+  sudo service udev restart
 }
 
 setup_android_udev
