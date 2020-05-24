4403666488a4e0a62a6895f09a1fe250d88ec491
Harsh Shandilya <me@msfjarvis.dev>
2020-05-24 21:34:00 +0530

    nixos: add $HOME/bin to $PATH
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -69,6 +69,7 @@ in
     xclip
     #linuxPackages_latest.rtl8821ce
   ];
+  environment.homeBinInPath = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
