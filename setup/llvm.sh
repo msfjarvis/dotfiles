8b28ff2dd4fd0964a2ebab9c9ef9cd8ae77623c0
Harsh Shandilya <me@msfjarvis.dev>
2020-06-29 20:59:48 +0530

    setup/llvm: add support for Ulyana
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- setup/llvm.sh
+++ setup/llvm.sh
@@ -48,7 +48,7 @@ case "$DIST_VERSION" in
   Ubuntu_18.10) REPO_NAME="deb http://apt.llvm.org/cosmic/   llvm-toolchain-cosmic$LLVM_VERSION_STRING  main" ;;
   Ubuntu_19.04) REPO_NAME="deb http://apt.llvm.org/disco/    llvm-toolchain-disco$LLVM_VERSION_STRING   main" ;;
   Ubuntu_19.10) REPO_NAME="deb http://apt.llvm.org/eoan/      llvm-toolchain-eoan$LLVM_VERSION_STRING    main" ;;
-  Ubuntu_20.04) REPO_NAME="deb http://apt.llvm.org/focal/     llvm-toolchain-focal$LLVM_VERSION_STRING    main" ;;
+  Linuxmint_20 | Ubuntu_20.04) REPO_NAME="deb http://apt.llvm.org/focal/     llvm-toolchain-focal$LLVM_VERSION_STRING    main" ;;
   *)
     echo "Distribution '$DISTRO' in version '$VERSION' is not supported by this script (${DIST_VERSION})."
     exit 2
