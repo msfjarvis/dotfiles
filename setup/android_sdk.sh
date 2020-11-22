2af89799c6d02dacdea7bdd299effead69365b3b
Harsh Shandilya <me@msfjarvis.dev>
2020-11-22 21:58:13 +0530

    setup/android_sdk: update file paths
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- setup/android_sdk.sh
+++ setup/android_sdk.sh
@@ -6,6 +6,8 @@
 # shellcheck source=setup/common.sh
 source "${SCRIPT_DIR:?}"/setup/common.sh
 
+trap 'rm -rf /tmp/tools.zip 2>/dev/null' INT TERM EXIT
+
 SDK_TOOLS=commandlinetools-linux-6609375_latest.zip
 
 function setup_android_sdk() {
@@ -13,12 +15,12 @@ function setup_android_sdk() {
   echoText "Installing Android SDK"
   mkdir -p "${SDK_DIR}"
   if [ ! -f "${SDK_TOOLS}" ]; then
-    wget https://dl.google.com/android/repository/"${SDK_TOOLS}" -O "${SDK_DIR}"/"${SDK_TOOLS}"
+    wget https://dl.google.com/android/repository/"${SDK_TOOLS}" -O /tmp/tools.zip
   fi
-  unzip -qo "${SDK_DIR}"/"${SDK_TOOLS}" -d "${SDK_DIR}"
+  unzip -qo /tmp/tools.zip -d "${SDK_DIR}"
   while read -r package; do
     yes | "${SDK_DIR}"/tools/bin/sdkmanager --sdk_root="${HOME:?}/Android/Sdk/" "${package:?}"
   done <"${SCRIPT_DIR}"/setup/sdk-packages-android.txt
-  rm "${SDK_TOOLS}"
+  rm /tmp/tools.zip
   cd - || exit
 }
