f289581f85c04bbef3f748e61b267470fd8bc656
Harsh Shandilya <me@msfjarvis.dev>
2020-11-22 21:21:07 +0530

    scripts: separate standalone installers
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -5,8 +5,5 @@
 
 set -euo pipefail
 
-# shellcheck source=common
-source "${SCRIPT_DIR:?}"/common
-
 echoText "Setting app switcher to current workspace only"
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
--- scripts/zulu-jdk.sh
+++ scripts/zulu-jdk.sh
@@ -7,9 +7,6 @@ set -euo pipefail
 
 trap "rm -f /tmp/zulu_jdk.deb 2>/dev/null" INT TERM EXIT
 
-# shellcheck source=common
-source "${SCRIPT_DIR:?}"/common
-
 ZULU_PACKAGE_URL="https://cdn.azul.com/zulu/bin/zulu14.27.1-ca-jdk14-linux_amd64.deb"
 ZULU_PACKAGE_CHECKSUM="16a7bad82c0f427f1896ec42914b67f5fc0b7841859c3b61b776c30773b44e78"
 
@@ -25,21 +22,17 @@ function verify_checksum() {
   fi
 }
 
-function check_and_install_jdk() {
-  echoText "Checking and installing Zulu JDK"
-  [ "$(java -version 2>&1 | head -n1 | sed 's/.*\"\(.*\)\"/\1/g')" == "14 2020-03-17" ] && {
-    reportWarning "Latest version of Zulu JDK already installed"
-    return 0
-  }
-  [ -f "/tmp/zulu_jdk.deb" ] && rm -f "/tmp/zulu_jdk.deb"
-  download_jdk
-  if verify_checksum; then
-    sudo dpkg -i "/tmp/zulu_jdk.deb"
-  else
-    reportError "Package checksum verification failed!"
-    return 1
-  fi
-  rm -f "/tmp/zulu_jdk.deb" 2>/dev/null
+echoText "Checking and installing Zulu JDK"
+[ "$(java -version 2>&1 | head -n1 | sed 's/.*\"\(.*\)\"/\1/g')" == "14 2020-03-17" ] && {
+  reportWarning "Latest version of Zulu JDK already installed"
+  return 0
 }
-
-check_and_install_jdk
+[ -f "/tmp/zulu_jdk.deb" ] && rm -f "/tmp/zulu_jdk.deb"
+download_jdk
+if verify_checksum; then
+  sudo dpkg -i "/tmp/zulu_jdk.deb"
+else
+  reportError "Package checksum verification failed!"
+  return 1
+fi
+rm -f "/tmp/zulu_jdk.deb" 2>/dev/null
