cef66a46cd8eae8f52e4b8e6a3e74a677d6333fb
Harsh Shandilya <me@msfjarvis.dev>
2021-01-18 14:29:56 +0530

    setup/android_sdk: fixup for latest command line tools
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- setup/00-android_sdk.sh
+++ setup/00-android_sdk.sh
@@ -8,11 +8,10 @@ source "${SCRIPT_DIR:?}"/setup/common.sh
 
 trap 'rm -rf /tmp/tools.zip 2>/dev/null' INT TERM EXIT
 
-SDK_TOOLS=commandlinetools-linux-6609375_latest.zip
+SDK_TOOLS=commandlinetools-linux-6858069_latest.zip
 
 function setup_android_sdk() {
   SDK_DIR="${HOME:?}/Android/Sdk"
-  [ -d "${SDK_DIR}/build-tools" ] && return
   echoText "Installing Android SDK"
   mkdir -p "${SDK_DIR}"
   if [ ! -f "${SDK_TOOLS}" ]; then
@@ -20,7 +19,7 @@ function setup_android_sdk() {
   fi
   unzip -qo /tmp/tools.zip -d "${SDK_DIR}"
   while read -r package; do
-    yes | "${SDK_DIR}"/tools/bin/sdkmanager --sdk_root="${HOME:?}/Android/Sdk/" "${package:?}"
+    yes | "${SDK_DIR}"/tools/bin/sdkmanager "${package:?}"
   done <"${SCRIPT_DIR}"/setup/sdk-packages-android.txt
   rm /tmp/tools.zip
   cd - || exit
