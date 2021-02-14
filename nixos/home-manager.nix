a276fe16d2aa99c0f78ef74c3e8cf0a46019a340
Harsh Shandilya <me@msfjarvis.dev>
2021-02-14 12:37:14 +0530

    nixos/home-manager: load system completions
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -16,7 +16,14 @@
     historyControl = [
       "ignorespace"
     ];
-    initExtra = "source ${config.home.homeDirectory}/git-repos/dotfiles/shell-init";
+    bashrcExtra = "
+    source ${config.home.homeDirectory}/git-repos/dotfiles/shell-init
+    if [ -f /usr/share/bash-completion/bash_completion ]; then
+      . /usr/share/bash-completion/bash_completion
+    elif [ -f /etc/bash_completion ]; then
+      . /etc/bash_completion
+    fi
+    ";
   };
 
   programs.bat = {
