2cbd2530243c360650b4071750282ab270f8abad
Harsh Shandilya <me@msfjarvis.dev>
2021-03-06 12:53:45 +0530

    nixos/setup-channels: add script for keeping nix channels in sync
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

---
+++ nixos/setup-channels.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Check if nix-channel is installed.
+if command -v nix-channel; then
+
+  # If channels exist already, remove all of them.
+  if [ -f "${HOME}/.nix-channels" ]; then
+    while read -r line; do
+      channel_name="$(echo $line | cut -d ' ' -f 2)"
+      nix-channel --remove "${channel_name}"
+    done < "${HOME}/.nix-channels"
+  fi
+
+  # Add the nix channels we're interested in
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+  nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
+
+  # Update channels
+  nix-channel --update
+fi
