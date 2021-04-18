3bc947a966cce3b1492ebed7036d8ea1b1c5da7d
Harsh Shandilya <me@msfjarvis.dev>
2021-04-19 01:15:11 +0530

    nixos/home-manager: bump custom-nixpkgs
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let customTarball = fetchTarball
-  "https://github.com/msfjarvis/custom-nixpkgs/archive/8c8f1688d183.tar.gz";
+  "https://github.com/msfjarvis/custom-nixpkgs/archive/1feb60da6b91.tar.gz";
 in
 {
   home.username = "msfjarvis";
