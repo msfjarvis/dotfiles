ea5ed2d98bb59d25285b803dc767c4d0ee152b85
Harsh Shandilya <me@msfjarvis.dev>
2021-08-28 16:01:08 +0530

    setup/android-sdk: reimplement directory logic

--- setup/00-android_sdk.sh
+++ setup/00-android_sdk.sh
@@ -11,21 +11,27 @@ trap 'rm -rf /tmp/tools.zip 2>/dev/null' INT TERM EXIT
 SDK_TOOLS=commandlinetools-linux-7583922_latest.zip
 
 function setup_android_sdk() {
+  local SDK_DIR TMP_DIR
   SDK_DIR="/mnt/mediahell/Android/Sdk"
+  TMP_DIR="$(mktemp -d)"
   if [ ! -d "${SDK_DIR}" ]; then
     mkdir -p "${SDK_DIR}"
   fi
   echoText "Installing Android SDK"
-  mkdir -p "${SDK_DIR}"
   if [ ! -f "${SDK_TOOLS}" ]; then
     wget https://dl.google.com/android/repository/"${SDK_TOOLS}" -O /tmp/tools.zip
   fi
-  unzip -qo /tmp/tools.zip -d "${SDK_DIR}"
+  unzip -qo /tmp/tools.zip -d "${TMP_DIR}"
+  mkdir -p "${SDK_DIR}"/cmdline-tools/latest/
+  pushd "${TMP_DIR}/cmdline-tools/"
+  cp -vR ./* "${SDK_DIR}"/cmdline-tools/latest/
+  popd
+  pushd "${SDK_DIR}"
   while read -r package; do
-    yes | "${SDK_DIR}"/cmdline-tools/bin/sdkmanager --sdk_root="${SDK_DIR}" "${package:?}"
+    yes | "${SDK_DIR}"/cmdline-tools/latest/bin/sdkmanager "${package:?}"
   done <"${SCRIPT_DIR}"/setup/sdk-packages-android.txt
-  rm /tmp/tools.zip
-  cd - || exit
+  popd
+  rm -rf /tmp/tools.zip "${TMP_DIR}"
 }
 
 if [[ ${BASH_SOURCE[0]} == "${0}" ]]; then
