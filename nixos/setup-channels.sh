1bbad1dfab51657783f9bb3317535c5af7a8e47a
Harsh Shandilya <me@msfjarvis.dev>
2021-09-10 02:51:31 +0530

    x: add setup-channels and format

--- nixos/setup-channels.sh
+++ nixos/setup-channels.sh
@@ -8,9 +8,9 @@ if command -v nix-channel; then
   # If channels exist already, remove all of them.
   if [ -f "${HOME}/.nix-channels" ]; then
     while read -r line; do
-      channel_name="$(echo $line | cut -d ' ' -f 2)"
+      channel_name="$(echo "$line" | cut -d ' ' -f 2)"
       nix-channel --remove "${channel_name}"
-    done < "${HOME}/.nix-channels"
+    done <"${HOME}/.nix-channels"
   fi
 
   # Add the nix channels we're interested in
