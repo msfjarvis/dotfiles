9e2f64a29fec16099bf0a6e042d29bfb5136165f
Harsh Shandilya <me@msfjarvis.dev>
2021-10-10 19:44:59 +0530

    nixos/home-manager: re-enable nix in topgrade

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -176,7 +176,7 @@ in {
     enable = pkgs.stdenv.isLinux;
 
     settings = {
-      disable = [ "sdkman" "node" "nix" ];
+      disable = [ "sdkman" "node" ];
 
       remote_topgrades = [ "backup" "ci" "oracle" ];
 
