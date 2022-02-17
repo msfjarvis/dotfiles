2a6ce2a69a7e4a654fe499601fdd23709ef4c9f9
Harsh Shandilya <me@msfjarvis.dev>
2022-02-17 12:30:45 +0530

    nixos/shell-configs: pin Rust version

--- nixos/shell-configs/rust.nix
+++ nixos/shell-configs/rust.nix
@@ -1,9 +1,8 @@
 with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
 mkShell {
   buildInputs = [
-    (rust-bin.selectLatestNightlyWith (toolchain:
-      toolchain.default.override {
-        extensions = [ "rust-src" "rustfmt-preview" ];
-      }))
+    (rust-bin.nightly."2022-02-14".default.override {
+      extensions = [ "rust-src" "rustfmt-preview" ];
+    })
   ];
 }
