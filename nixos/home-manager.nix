65eb851434f300bdce8d364ba70adef95f5bd58a
Harsh Shandilya <me@msfjarvis.dev>
2022-03-29 16:25:52 +0530

    nixos: switch back to nightly Rust

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -312,13 +312,14 @@ in {
       cowsay
       dnscontrol
       fclones
-      (rust-bin.stable.latest.default.override {
-        extensions = [ "rust-src" "rustfmt-preview" ];
-        targets =
-          pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
-          ++ pkgs.lib.optionals pkgs.stdenv.isLinux
-          [ "x86_64-unknown-linux-musl" ];
-      })
+      (rust-bin.selectLatestNightlyWith (toolchain:
+        toolchain.default.override {
+          extensions = [ "rust-src" "rustfmt-preview" "llvm-tools-preview" ];
+          targets =
+            pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isLinux
+            [ "x86_64-unknown-linux-musl" ];
+        }))
       ffmpeg
       figlet
       custom.gdrive
--- nixos/shell-configs/rust.nix
+++ nixos/shell-configs/rust.nix
@@ -1,12 +1,13 @@
 with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
 mkShell {
   buildInputs = [
-    (rust-bin.stable.latest.default.override {
-      extensions = [ "rust-src" "rustfmt-preview" ];
-      targets =
-        pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
-        ++ pkgs.lib.optionals pkgs.stdenv.isLinux
-        [ "x86_64-unknown-linux-musl" ];
-    })
+    (rust-bin.selectLatestNightlyWith (toolchain:
+      toolchain.default.override {
+        extensions = [ "rust-src" "rustfmt-preview" "llvm-tools-preview" ];
+        targets =
+          pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isLinux
+          [ "x86_64-unknown-linux-musl" ];
+      }))
   ];
 }
