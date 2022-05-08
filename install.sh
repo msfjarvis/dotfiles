cafb1f8daccd5a27a6ba9b459bfb6ffd20e04882
Harsh Shandilya <me@msfjarvis.dev>
2022-05-08 13:04:16 +0530

    nanorc: remove

--- install.sh
+++ install.sh
@@ -31,21 +31,6 @@ done
 
 cd "${SCRIPT_DIR}" || exit 1
 
-echoText 'Installing nanorc'
-cp -v "${SCRIPT_DIR}"/.nanorc ~/.nanorc
-if [ ! -d "${HOME}/.nano" ]; then
-  git clone https://github.com/scopatz/nanorc ~/.nano -b master
-else
-  git -C "${HOME}/.nano" pull --rebase
-fi
-
-# add all includes from ~/.nano/nanorc if they're not already there
-while read -r inc; do
-  if ! grep -q "$inc" ~/.nanorc; then
-    echo "$inc" >>~/.nanorc
-  fi
-done <~/.nano/nanorc
-
 echoText "Moving credentials"
 gpg --decrypt "${SCRIPT_DIR}"/.secretcreds.gpg >~/.secretcreds
 
