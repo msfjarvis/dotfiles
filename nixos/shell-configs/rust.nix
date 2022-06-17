bfe59d705877210a2eba37ab12a61f06a6f09ddb
Harsh Shandilya <me@msfjarvis.dev>
2022-06-17 12:38:06 +0530

    nixos/shell-configs: add clippy to rust extensions

--- nixos/shell-configs/rust.nix
+++ nixos/shell-configs/rust.nix
@@ -2,8 +2,13 @@ with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
 mkShell {
   buildInputs = [
     (rust-bin.stable.latest.default.override {
-      extensions =
-        [ "rust-src" "rustc-dev" "rustfmt-preview" "llvm-tools-preview" ];
+      extensions = [
+        "clippy"
+        "llvm-tools-preview"
+        "rust-src"
+        "rustc-dev"
+        "rustfmt-preview"
+      ];
       targets =
         pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
         ++ pkgs.lib.optionals pkgs.stdenv.isLinux
