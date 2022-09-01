8b9a971384559f099b09af703435b8d72b83dbe7
Harsh Shandilya <me@msfjarvis.dev>
2022-09-01 18:38:52 +0530

    install: cleanup and simplify

--- install.sh
+++ install.sh
@@ -14,40 +14,20 @@ source "${SCRIPT_DIR}"/system
 
 trap 'exit 1' INT TERM
 
-declare -a SCRIPTS=("paste")
+if [[ -n ${RUN_SETUP} ]]; then
 
-# Create binaries directory
-mkdir -p ~/bin/
-
-for i in "${SCRIPT_DIR}"/setup/*.sh; do
-  name="$(basename "${i/.sh/}")"
-  normalized_name="${name//[[:digit:]]/}"
-  normalized_name="${normalized_name/-/}"
-  # shellcheck disable=SC1090
-  . "$i"
-  setup_"$normalized_name"
-  unset name setup_"$normalized_name"
-done
-
-cd "${SCRIPT_DIR}" || exit 1
-
-echoText "Moving credentials"
-gpg --decrypt "${SCRIPT_DIR}"/.secretcreds.gpg >~/.secretcreds
-
-if [ -f ~/.bashrc ]; then
-  ret="$(grep -qF "shell-init" ~/.bashrc)"
-  if [ "${ret}" ]; then
-    reportWarning "shell-init is not sourced in the bashrc, appending"
-    echo "source ${SCRIPT_DIR}/shell-init" >>~/.bashrc
-  fi
+  for i in "${SCRIPT_DIR}"/setup/*.sh; do
+    name="$(basename "${i/.sh/}")"
+    normalized_name="${name//[[:digit:]]/}"
+    normalized_name="${normalized_name/-/}"
+    # shellcheck disable=SC1090
+    . "$i"
+    setup_"$normalized_name"
+    unset name setup_"$normalized_name"
+  done
 fi
 
-echoText "Installing scripts"
-for SCRIPT in "${SCRIPTS[@]}"; do
-  echo -e "${CL_YLW}Processing ${SCRIPT}${CL_RST}"
-  rm -rf ~/bin/"${SCRIPT}"
-  ln -s "${SCRIPT_DIR}"/"${SCRIPT}" ~/bin/"${SCRIPT}"
-done
+cd "${SCRIPT_DIR}" || exit 1
 
 echoText "Setting up gitconfig"
 cp "${SCRIPT_DIR}/.gitconfig" ~/.gitconfig
