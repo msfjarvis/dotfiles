1447e46289fe14d75ece26d67875b07abb6c499d
Harsh Shandilya <me@msfjarvis.dev>
2022-12-03 12:09:58 +0530

    fix(nix): switch to `nixpkgs-unstable` branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configurations of Harsh Shandilya";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
