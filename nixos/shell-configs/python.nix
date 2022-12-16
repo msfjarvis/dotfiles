32992eeef02851a47eac4789b7d75855d919d5df
Harsh Shandilya <me@msfjarvis.dev>
2022-12-16 16:16:39 +0530

    nixos/shell-configs: add python

---
+++ nixos/shell-configs/python.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ python310 python310Packages.poetry ];
+        };
+      });
+}
