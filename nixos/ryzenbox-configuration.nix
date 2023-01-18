24fdd8aa453211702b9d2763d9ad6f64187f633e
Harsh Shandilya <me@msfjarvis.dev>
2023-01-18 17:27:47 +0530

    nixos/ryzenbox: embed extensions directly into `pass`

--- nixos/ryzenbox-configuration.nix
+++ nixos/ryzenbox-configuration.nix
@@ -156,7 +156,10 @@
     enableBashIntegration = true;
   };
 
-  programs.password-store = {enable = true;};
+  programs.password-store = {
+    enable = true;
+    package = pkgs.pass.withExtensions (exts: [exts.pass-audit exts.pass-genphrase exts.pass-otp exts.pass-update]);
+  };
 
   programs.starship = {
     enable = true;
