1971a0b51a6c10ad714f31d3e54945d3c5e71562
Harsh Shandilya <me@msfjarvis.dev>
2023-03-04 12:17:06 +0530

    nixos/shell-configs: make Rust config use Crane

--- nixos/shell-configs/rust.nix
+++ nixos/shell-configs/rust.nix
@@ -1,7 +1,11 @@
 {
+  description = "My Rust Project";
+
   inputs = {
     nixpkgs = {url = "github:NixOS/nixpkgs/nixpkgs-unstable";};
+
     flake-utils = {url = "github:numtide/flake-utils";};
+
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -14,12 +18,29 @@
         flake-utils.follows = "flake-utils";
       };
     };
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        flake-compat.follows = "flake-compat";
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+      };
+    };
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    crane,
     flake-utils,
+    advisory-db,
     rust-overlay,
     ...
   }:
@@ -28,18 +49,46 @@
         inherit system;
         overlays = [(import rust-overlay)];
       };
-      rustStable = pkgs.rust-bin.stable.latest.default.override {
-        extensions = ["rust-src"];
-        targets =
-          pkgs.lib.optionals pkgs.stdenv.isDarwin ["aarch64-apple-darwin"]
-          ++ pkgs.lib.optionals pkgs.stdenv.isLinux
-          ["x86_64-unknown-linux-gnu"];
+
+      rustStable =
+        pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
+      src = ./.;
+      cargoArtifacts = craneLib.buildDepsOnly {inherit src buildInputs;};
+      buildInputs = [];
+
+      my-rust-package = craneLib.buildPackage {
+        inherit src;
+        doCheck = false;
+      };
+      my-rust-package-clippy = craneLib.cargoClippy {
+        inherit cargoArtifacts src buildInputs;
+        cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+      };
+      my-rust-package-fmt = craneLib.cargoFmt {inherit src;};
+      my-rust-package-audit = craneLib.cargoAudit {inherit src advisory-db;};
+      my-rust-package-nextest = craneLib.cargoNextest {
+        inherit cargoArtifacts src buildInputs;
+        partitions = 1;
+        partitionType = "count";
       };
     in {
+      checks = {
+        inherit my-rust-package my-rust-package-audit my-rust-package-clippy my-rust-package-fmt my-rust-package-nextest;
+      };
+
+      packages.default = my-rust-package;
+
+      apps.default = flake-utils.lib.mkApp {drv = my-rust-package;};
+
       devShells.default = pkgs.mkShell {
         inputsFrom = builtins.attrValues self.checks;
 
-        nativeBuildInputs = [rustStable];
+        nativeBuildInputs = with pkgs; [
+          cargo-nextest
+          cargo-release
+          rustStable
+        ];
       };
     });
 }
