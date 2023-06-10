6cc6e850fbf142397738f0b95a6e40966bbdf2c5
Harsh Shandilya <me@msfjarvis.dev>
2023-06-11 02:33:19 +0530

    feat(flakes): add nixConfig

--- flake.nix
+++ flake.nix
@@ -129,4 +129,12 @@
       ];
     };
   };
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
 }
