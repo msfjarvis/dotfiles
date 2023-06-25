3fe771cf71c6baa04d97b1a4e34e6682dcae452b
Harsh Shandilya <me@msfjarvis.dev>
2023-06-25 12:26:21 +0530

    nixos/shell-configs: fix flake-compat URLs

--- nixos/shell-configs/default.nix
+++ nixos/shell-configs/default.nix
@@ -2,7 +2,7 @@
   lock = builtins.fromJSON (builtins.readFile ./flake.lock);
 in
   fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    url = "https://github.com/nix-community/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
     sha256 = lock.nodes.flake-compat.locked.narHash;
   }) {src = ./.;})
 .defaultNix
