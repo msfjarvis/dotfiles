3351f1be6ec33c0e9aa67b0a91ef7cdd15467ae4
Harsh Shandilya <me@msfjarvis.dev>
2023-07-02 23:12:03 +0530

    fix(nix-shell/node): adopt nix-systems and devshell

--- nixos/shell-configs/node.nix
+++ nixos/shell-configs/node.nix
@@ -1,23 +1,44 @@
 {
-  inputs = {
-    nixpkgs = {url = "github:NixOS/nixpkgs/nixpkgs-unstable";};
-    flake-utils = {url = "github:numtide/flake-utils";};
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  inputs.systems.url = "github:msfjarvis/flake-systems";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.devshell.inputs.systems.follows = "systems";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-utils.inputs.systems.follows = "systems";
+
+  inputs.flake-compat.url = "github:nix-community/flake-compat";
+  inputs.flake-compat.flake = false;
 
   outputs = {
     self,
     nixpkgs,
+    devshell,
     flake-utils,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [devshell.overlays.default];
+      };
     in {
-      devShells.default =
-        pkgs.mkShell {nativeBuildInputs = with pkgs; [nodejs-16_x];};
+      devShells.default = pkgs.devshell.mkShell {
+        bash = {interactive = "";};
+
+        env = [
+          {
+            name = "DEVSHELL_NO_MOTD";
+            value = 1;
+          }
+        ];
+
+        packages = with pkgs; [
+          nodejs-16_x
+        ];
+      };
     });
 }
