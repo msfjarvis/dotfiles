db746d4d346a862653cbad8b179f85c4efe4a411
Harsh Shandilya <me@msfjarvis.dev>
2023-08-01 14:30:28 +0530

    nixos: extract nixGLWrap to its own module

--- nixos/ryzenbox-configuration.nix
+++ nixos/ryzenbox-configuration.nix
@@ -3,26 +3,9 @@
   pkgs,
   ...
 }: let
-  nixGLWrap = binary: drv:
-    pkgs.symlinkJoin {
-      name = "${drv.name}-nixglwrapped";
-      paths = [drv];
-      nativeBuildInputs = [pkgs.makeWrapper];
-      postBuild = ''
-        # This will break if wrapProgram is ever changed, so fingers crossed
-        makeShellWrapper() {
-          local original="$1"
-          local wrapper="$2"
-          cat << EOF > "$wrapper"
-        #! ${pkgs.bash}/bin/bash -e
-        exec "${pkgs.nixgl.auto.nixGLDefault}/bin/nixGL" "$original"
-        EOF
-          chmod +x "$wrapper"
-        }
-
-        wrapProgram "$out/bin/${binary}"
-      '';
-    };
+  nixGLWrap = import ./modules/nixGL.nix {
+    inherit pkgs;
+  };
 in {
   imports = [./modules/vscode/home-manager.nix];
   home.username = "msfjarvis";
