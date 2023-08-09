cd60c699f12142cb4d469a3c885baab559667c27
Harsh Shandilya <me@msfjarvis.dev>
2023-08-10 02:32:58 +0530

    nixos/vscode: add GitHub Actions extension

--- nixos/modules/vscode/extensions.nix
+++ nixos/modules/vscode/extensions.nix
@@ -7,12 +7,6 @@
       sha256 = "1xi8wm0mqgld791xrba030hahsxzzw2lhx0ra2cvbc1wr8s5l8cr";
     }
     {
-      name = "remote-ssh-edit";
-      publisher = "ms-vscode-remote";
-      version = "0.86.0";
-      sha256 = "0cmh2d73y1kmp6a92h3z7gams7lnqvb7rgib52kqslm4hyhdmii6";
-    }
-    {
       name = "daybreak-theme";
       publisher = "mtdmali";
       version = "1.2.0";
@@ -23,6 +17,18 @@
       publisher = "charliermarsh";
       version = "2023.32.0";
       sha256 = "113irzc213z7hcwzhc8cg2fgl7wm2jws1imv2n9fphwig6ayivj2";
+    }
+    {
+      name = "remote-ssh-edit";
+      publisher = "ms-vscode-remote";
+      version = "0.86.0";
+      sha256 = "0cmh2d73y1kmp6a92h3z7gams7lnqvb7rgib52kqslm4hyhdmii6";
+    }
+    {
+      name = "vscode-github-actions";
+      publisher = "GitHub";
+      version = "0.26.1";
+      sha256 = "08imjlf761scl91fh6384yi29crgqv0g1646xhk92zfzw54xjpi1";
     }
     {
       name = "vscode-typescript-next";
