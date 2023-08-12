b689fdf3887e0738daa7e75ddfd31df7b17abc82
Harsh Shandilya <me@msfjarvis.dev>
2023-08-12 17:44:38 +0530

    nixos/crusty: mount media disk on boot

--- nixos/hosts/crusty/hardware-configuration.nix
+++ nixos/hosts/crusty/hardware-configuration.nix
@@ -17,6 +17,14 @@
     device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
     fsType = "ext4";
   };
+  fileSystems."/media" = {
+    device = "/dev/disk/by-uuid/7EC439F83DA97A5E";
+    fsType = "ntfs";
+    options = [
+      "allow_other"
+      "nofail"
+    ];
+  };
 
   swapDevices = [];
 
