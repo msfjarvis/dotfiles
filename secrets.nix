deef96c1956f3d508712386f0253de23ce3eee77
Harsh Shandilya <me@msfjarvis.dev>
2023-09-24 03:02:46 +0530

    secrets: drop cachix-deploy

--- secrets.nix
+++ secrets.nix
@@ -6,6 +6,5 @@ let
   ryzenbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM4wCylRGCmNivW6tlPn0tyNSyFN8WJa3CHFn7xsGDfV";
   systems = [crusty ryzenbox];
 in {
-  "secrets/crusty-cachix-deploy.age".publicKeys = [crusty];
   "secrets/crusty-transmission-settings.age".publicKeys = [crusty];
 }
