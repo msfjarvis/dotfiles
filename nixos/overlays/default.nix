3385a766e56632740d295b042cc7b8405173a324
Harsh Shandilya <me@msfjarvis.dev>
2023-10-12 11:42:55 +0530

    nixos: document overlays

--- nixos/overlays/default.nix
+++ nixos/overlays/default.nix
@@ -1,12 +1,16 @@
 final: prev: {
+  # Prevent using NixOS JDK, we'll use ours.
   maestro = prev.maestro.overrideAttrs (old: {
     postFixup = "";
   });
+  # Silence warnings about existing files
   megatools = prev.megatools.overrideAttrs (old: {
     patches = [./megatools.patch];
   });
+  # Set default fonts
   nerdfonts = prev.nerdfonts.override {
     fonts = ["CascadiaCode" "FiraCode" "Inconsolata" "JetBrainsMono"];
   };
+  # Drop patch that enforces software rendering
   scrcpy = prev.scrcpy.overrideAttrs (old: {postPatch = "";});
 }
