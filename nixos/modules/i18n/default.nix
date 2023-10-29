727562da8abe9d4842d87741b8febf930c4c7d32
Harsh Shandilya <me@msfjarvis.dev>
2023-10-29 17:57:37 +0530

    nixos/i18n: switch everything over to en_IN

--- nixos/modules/i18n/default.nix
+++ nixos/modules/i18n/default.nix
@@ -1,12 +1,16 @@
 _: {
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_IN";
+  i18n.supportedLocales = ["en_IN/UTF-8"];
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
+    LANGUAGE = "en_IN";
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
   };
 }
