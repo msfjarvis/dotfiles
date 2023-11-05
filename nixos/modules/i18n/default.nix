9ef5677aebfa67fb8d23f62f2fd2506484058a60
Harsh Shandilya <me@msfjarvis.dev>
2023-11-05 20:56:23 +0530

    nixos/i18n: update `LC_{NUMERIC,TIME}` value

--- nixos/modules/i18n/default.nix
+++ nixos/modules/i18n/default.nix
@@ -1,16 +1,17 @@
 _: {
   i18n.defaultLocale = "en_IN";
-  i18n.supportedLocales = ["en_IN/UTF-8"];
+  i18n.supportedLocales = ["en_IN/UTF-8" "en_US.UTF-8/UTF-8"];
   i18n.extraLocaleSettings = {
     LANGUAGE = "en_IN";
     LC_ADDRESS = "en_IN";
+    LC_ALL = "en_IN";
     LC_IDENTIFICATION = "en_IN";
     LC_MEASUREMENT = "en_IN";
     LC_MONETARY = "en_IN";
     LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
+    LC_NUMERIC = "en_US";
     LC_PAPER = "en_IN";
     LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
+    LC_TIME = "en_US";
   };
 }
