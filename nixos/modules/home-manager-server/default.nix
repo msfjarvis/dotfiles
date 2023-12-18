e8610df232fe14cbbb7b5296afe95d8c006379a7
Harsh Shandilya <me@msfjarvis.dev>
2023-12-18 12:43:05 +0530

    nixos/home-manager-server: simplify

--- nixos/modules/home-manager-server/default.nix
+++ nixos/modules/home-manager-server/default.nix
@@ -1,18 +1,4 @@
-{config, ...}: {
-  programs.bash = {
-    historyFile = "${config.home.homeDirectory}/.bash_history";
-    initExtra = ''
-      # Source shell-init from my dotfiles
-      source ${config.home.homeDirectory}/dotfiles/shell-init
-    '';
-  };
-
-  programs.git = {
-    includes = [
-      {path = "${config.home.homeDirectory}/dotfiles/.gitconfig";}
-    ];
-  };
-
+_: {
   programs.starship = {
     settings = {
       format = "$directory$git_branch$git_state$git_status➜ ";
