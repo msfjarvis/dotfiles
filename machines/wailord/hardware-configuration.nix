8875f09b51c277bb01fe4f00c9f4fd7075e4b6ca
Harsh Shandilya <me@msfjarvis.dev>
2023-12-22 18:59:57 +0530

    wailord: update for netcup deployment

--- machines/wailord/hardware-configuration.nix
+++ machines/wailord/hardware-configuration.nix
@@ -1,10 +1,18 @@
 {modulesPath, ...}: {
   imports = [(modulesPath + "/profiles/qemu-guest.nix")];
-  boot.loader.grub.device = "/dev/vda";
-  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
+  boot.loader.grub = {
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    device = "nodev";
+  };
+  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront"];
   boot.initrd.kernelModules = ["nvme"];
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/CA1A-B46F";
+    fsType = "vfat";
+  };
   fileSystems."/" = {
-    device = "/dev/vda1";
+    device = "/dev/vda4";
     fsType = "ext4";
   };
 }
