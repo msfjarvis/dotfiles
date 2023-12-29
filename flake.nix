310790e3da6a28270818cf5ac788d1e4761c3fca
Harsh Shandilya <me@msfjarvis.dev>
2023-12-30 00:49:06 +0530

    nix: patch home-manager to only attempt migration when required

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.flake-utils.inputs.systems.follows = "systems";
 
-  inputs.home-manager.url = "github:nix-community/home-manager/master";
+  inputs.home-manager.url = "github:msfjarvis/home-manager?rev=2ebc37a33a74f71f676def6faa2e60a1d7293119";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.nix-index-database.url = "github:nix-community/nix-index-database";
