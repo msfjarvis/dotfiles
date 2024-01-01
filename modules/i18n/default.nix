8cce5f7dd3afe86a25a0f87cdaecaaf79ceb8fcc
Harsh Shandilya <me@msfjarvis.dev>
2024-01-01 11:18:23 +0530

    i18n: move to `en_US.UTF-8` locale

--- modules/i18n/default.nix
+++ modules/i18n/default.nix
@@ -1,17 +1,16 @@
 {lib, ...}: {
-  i18n.defaultLocale = lib.mkDefault "en_IN";
-  i18n.supportedLocales = lib.mkDefault ["en_IN/UTF-8" "en_US.UTF-8/UTF-8"];
+  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+  i18n.supportedLocales = lib.mkDefault ["en_US.UTF-8/UTF-8"];
   i18n.extraLocaleSettings = lib.mkDefault {
-    LANGUAGE = "en_IN";
-    LC_ADDRESS = "en_IN";
-    LC_ALL = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_US";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_US";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 }
