bb3667b02efcc4a8f34f705424ce441aab2b6cc9
Harsh Shandilya <me@msfjarvis.dev>
2024-01-11 00:15:30 +0530

    home-manager: enable wayland support in pass

--- home-manager/password-store/default.nix
+++ home-manager/password-store/default.nix
@@ -5,6 +5,11 @@
 }: {
   programs.password-store = {
     enable = lib.mkDefault true;
-    package = lib.mkDefault (pkgs.pass.withExtensions (exts: [exts.pass-audit exts.pass-genphrase exts.pass-otp exts.pass-update]));
+    package = lib.mkDefault ((pkgs.pass.override {
+        x11Support = false;
+        waylandSupport = true;
+        dmenuSupport = false;
+      })
+      .withExtensions (exts: [exts.pass-audit exts.pass-genphrase exts.pass-otp exts.pass-update]));
   };
 }
