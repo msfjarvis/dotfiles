ccd476592fa7bb3d5a200ea51a86571a413fc750
Harsh Shandilya <me@msfjarvis.dev>
2024-01-17 16:24:42 +0530

    modules/nix: thingamabob

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -1,31 +1,55 @@
 {
   lib,
+  pkgs,
+  config,
   inputs,
   ...
-}: let
-  inherit (lib) filterAttrs;
-  flakes = filterAttrs (_name: value: value ? outputs) inputs;
+}: {
+  documentation = {
+    enable = true;
+    doc.enable = false;
+    man.enable = true;
+    dev.enable = false;
+  };
 
-  nixRegistry =
-    builtins.mapAttrs
-    (_name: v: {flake = v;})
-    flakes;
-in {
   nix = {
     gc = {
       automatic = true;
-      dates = "weekly";
-      persistent = true;
+      dates = "daily";
+      options = "--delete-older-than 3d";
     };
-    registry = nixRegistry;
+    package = pkgs.nixUnstable;
+
+    registry = lib.mapAttrs (_: v: {flake = v;}) inputs;
+
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    extraOptions = ''
+      experimental-features = nix-command flakes recursive-nix
+      keep-outputs = true
+      warn-dirty = false
+      keep-derivations = true
+    '';
     settings = {
+      flake-registry = "/etc/nix/registry.json";
+      auto-optimise-store = true;
+      builders-use-substitutes = true;
+      allowed-users = ["@wheel"];
+      trusted-users = ["@wheel" "msfjarvis"];
+      sandbox = true;
+      max-jobs = "auto";
+      keep-going = true;
+      log-lines = 20;
+      extra-experimental-features = ["flakes" "nix-command" "recursive-nix" "ca-derivations"];
+
       trusted-substituters = [
+        "https://cache.garnix.io"
         "https://msfjarvis.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "msfjarvis.cachix.org-1:/sKPgZblk/LgoOKtDgMTwvRuethILGkr/maOvZ6W11U="
       ];
-      trusted-users = ["msfjarvis" "root"];
     };
   };
 }
