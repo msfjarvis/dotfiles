cb345923e40380b80169412c5ebdb51edec0339a
Harsh Shandilya <me@msfjarvis.dev>
2024-01-19 00:31:16 +0530

    nix: make ffmpeg usable

--- machines/ryzenbox/default.nix
+++ machines/ryzenbox/default.nix
@@ -55,7 +55,24 @@
       dracula-icon-theme
       dracula-theme
       fclones
-      ffmpeg
+      (ffmpeg.override {
+        buildFfplay = false;
+        buildFfprobe = false;
+        buildQtFaststart = false;
+        withDav1d = true;
+        withMp3lame = true;
+        withAss = true;
+        withDrm = true;
+        withVaapi = true;
+        withVdpau = true;
+        withVorbis = true;
+        withVpx = true;
+        withWebp = true;
+        withNvdec = true;
+        withNvenc = true;
+        withX264 = true;
+        withX265 = true;
+      })
       firefox
       fzf
       gdrive
