e37da92ab9f2dae95fd32d4b736d1ffa48955cba
Harsh Shandilya <me@msfjarvis.dev>
2024-01-23 15:38:46 +0530

    ryzenbox: use a custom mpv build

--- machines/ryzenbox/default.nix
+++ machines/ryzenbox/default.nix
@@ -16,6 +16,53 @@
   # boot.loader.efi.canTouchEfiVariables = true;
 
   home-manager.users.msfjarvis = {
+    programs.mpv = {
+      enable = true;
+      package =
+        pkgs.wrapMpv (pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+          x11Support = false;
+          cddaSupport = false;
+          vulkanSupport = false;
+          drmSupport = false;
+          archiveSupport = false;
+          bluraySupport = false;
+          bs2bSupport = false;
+          cacaSupport = false;
+          cmsSupport = false;
+          dvdnavSupport = false;
+          dvbinSupport = false;
+          jackaudioSupport = false;
+          javascriptSupport = false;
+          libpngSupport = false;
+          openalSupport = false;
+          pulseSupport = false;
+          pipewireSupport = true;
+          rubberbandSupport = false;
+          screenSaverSupport = false;
+          sdl2Support = true;
+          sixelSupport = false;
+          speexSupport = false;
+          swiftSupport = false;
+          theoraSupport = false;
+          vaapiSupport = true;
+          vapoursynthSupport = false;
+          vdpauSupport = true;
+          xineramaSupport = false;
+          xvSupport = false;
+          zimgSupport = false;
+        }) {
+          scripts = with pkgs.mpvScripts; [
+            uosc
+            thumbfast
+            inhibit-gnome
+          ];
+        };
+      config = {
+        geometry = "50%:50%";
+        autofit-larger = "100%:100%";
+      };
+    };
     stylix = {
       targets = {
         firefox = {
@@ -52,7 +99,6 @@
       adb-sync
       adx
       age
-      celluloid
       diffuse-bin
       dracula-icon-theme
       dracula-theme
