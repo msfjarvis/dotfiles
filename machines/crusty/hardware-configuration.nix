fb32a561811a1bebf3f9bc5efaa27608924847a8
Harsh Shandilya <me@msfjarvis.dev>
2024-01-24 16:13:07 +0530

    crusty: add back the media disk

--- machines/crusty/hardware-configuration.nix
+++ machines/crusty/hardware-configuration.nix
@@ -8,4 +8,12 @@
   ];
   boot.supportedFilesystems = ["ntfs"];
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+
+  fileSystems."/media" = {
+    device = "/dev/disk/by-uuid/1d9cbc92-8ea9-4ae7-8c8f-5f72c3d75626";
+    fsType = "ext4";
+    options = [
+      "nofail"
+    ];
+  };
 }
