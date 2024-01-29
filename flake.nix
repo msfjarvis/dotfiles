f41f32e56fb7ecef1a72d3b7c9f1b6fbed5786a6
Harsh Shandilya <me@msfjarvis.dev>
2024-01-29 16:45:35 +0530

    flake.lock: move back to upstream nixpkgs-unstable
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:msfjarvis/nixpkgs/602a6e9f6e43f81eb376cabc9d0d3f72eca60776' (2024-01-27)
      → 'github:NixOS/nixpkgs/160b762eda6d139ac10ae081f8f78d640dd523eb' (2024-01-27)

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "msfjarvis' NixOS configurations";
 
-  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.nixpkgs.url = "github:msfjarvis/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  # inputs.nixpkgs.url = "github:msfjarvis/nixpkgs/nixpkgs-unstable";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
   inputs.systems.url = "github:msfjarvis/flake-systems";
