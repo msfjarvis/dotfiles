26a9e66d96e9bbf25087a04573412fa5cd3c91ef
Harsh Shandilya <me@msfjarvis.dev>
2024-02-11 15:43:27 +0530

    modules: add gphotos-cdp

---
+++ modules/gphotos-cdp/default.nix
@@ -0,0 +1,72 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.services.gphotos-cdp;
+in {
+  options.services.gphotos-cdp = {
+    enable = mkEnableOption {
+      description = mdDoc "Whether to enable the gphotos-cdp backup service.";
+    };
+
+    session-dir = mkOption {
+      type = types.str;
+      description = mdDoc "Path to the Google Chrome user session directory";
+    };
+
+    dldir = mkOption {
+      type = types.str;
+      description = mdDoc "Path to the directory where the files are downloaded";
+    };
+
+    user = mkOption {
+      type = types.str;
+      default = "gphotos-cdp";
+      description = mdDoc "User account under which gphotos-cdp runs.";
+    };
+
+    group = mkOption {
+      type = types.str;
+      default = "gphotos-cdp";
+      description = mdDoc "Group account under which gphotos-cdp runs.";
+    };
+
+    package = mkPackageOptionMD pkgs "gphotos-cdp" {};
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.gphotos-cdp = {
+      wantedBy = ["default.target"];
+      after = ["fs.service" "networking.target"];
+      wants = ["fs.service" "networking.target"];
+
+      serviceConfig = {
+        User = cfg.user;
+        Group = cfg.group;
+        Restart = "on-failure";
+        RestartSec = "30s";
+        Type = "simple";
+        Environment = "PATH=${makeBinPath [pkgs.coreutils pkgs.google-chrome]}";
+      };
+      script = ''
+        exec env ${getExe cfg.package} -v -dev -headless -dldir ${cfg.dldir} -session-dir ${cfg.session-dir}
+      '';
+    };
+
+    users.users = mkIf (cfg.user == "gphotos-cdp") {
+      gphotos-cdp = {
+        inherit (cfg) group;
+        home = cfg.dldir;
+        createHome = false;
+        description = "gphotos-cdp daemon user";
+        isNormalUser = true;
+      };
+    };
+
+    users.groups =
+      mkIf (cfg.group == "gphotos-cdp") {gphotos-cdp = {gid = null;};};
+  };
+}
