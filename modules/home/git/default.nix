a2010687bd0b91c7ce35038800b184850dc53608
Harsh Shandilya <me@msfjarvis.dev>
2024-03-01 00:35:34 +0530

    modules/home/git: work work work work work

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -1,8 +1,11 @@
 {
   config,
   lib,
+  host,
   ...
-}: {
+}: let
+  isWorkMachine = host == "Harshs-MacBook-Pro";
+in {
   programs.git = {
     enable = true;
     ignores = [
@@ -14,8 +17,12 @@
       "fleet.toml"
       ".DS_Store"
     ];
-    includes = lib.mkDefault [
-      {path = "${config.home.homeDirectory}/git-repos/dotfiles/.gitconfig";}
-    ];
+    includes =
+      [
+        {path = "${config.home.homeDirectory}/git-repos/dotfiles/.gitconfig";}
+      ]
+      ++ lib.optionals isWorkMachine [
+        {path = "${config.home.homeDirectory}/git-repos/dotfiles/.gitconfig-work";}
+      ];
   };
 }
