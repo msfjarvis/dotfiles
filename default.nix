3f65a782a429433011b9b2d84332f23bf0caad32
Harsh Shandilya <me@msfjarvis.dev>
2024-03-05 17:21:27 +0530

    nix: setup for nix-update

---
+++ default.nix
@@ -0,0 +1,95 @@
+let
+  inherit
+    (builtins)
+    currentSystem
+    fromJSON
+    readFile
+    ;
+
+  # Copied from https://github.com/edolstra/flake-compat/pull/44/files
+  fetchurl = {
+    url,
+    sha256,
+  }:
+    derivation {
+      builder = "builtin:fetchurl";
+
+      name = "source";
+      inherit url;
+
+      outputHash = sha256;
+      outputHashAlgo = "sha256";
+      outputHashMode = "recursive";
+      executable = false;
+      unpack = false;
+
+      system = "builtin";
+
+      # No need to double the amount of network traffic
+      preferLocalBuild = true;
+
+      impureEnvVars = [
+        # We borrow these environment variables from the caller to allow
+        # easy proxy configuration.  This is impure, but a fixed-output
+        # derivation like fetchurl is allowed to do so since its result is
+        # by definition pure.
+        "http_proxy"
+        "https_proxy"
+        "ftp_proxy"
+        "all_proxy"
+        "no_proxy"
+      ];
+
+      # To make "nix-prefetch-url" work.
+      urls = [url];
+    };
+
+  getFlake = name:
+    with (fromJSON (readFile ./flake.lock)).nodes.${name}.locked; {
+      inherit rev;
+      outPath = fetchTarball {
+        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+        sha256 = narHash;
+      };
+    };
+  getRawFlake = name:
+    with (fromJSON (readFile ./flake.lock)).nodes.${name}.locked; {
+      inherit rev;
+      outPath = fetchurl {
+        inherit url;
+        sha256 = narHash;
+      };
+    };
+in
+  {
+    system ? currentSystem,
+    pkgs ? import (getFlake "nixpkgs") {localSystem = {inherit system;};},
+    lib ? pkgs.lib,
+    fenix ? getFlake "fenix",
+    rust-manifest ? getRawFlake "rust-manifest",
+  }: let
+    callPackage = pkg: pkgs.callPackage pkg;
+  in {
+    adb-sync = callPackage ./packages/adb-sync {};
+    adbtuifm = callPackage ./packages/adbtuifm {};
+    adx = callPackage ./packages/adx {};
+    clipboard-substitutor = callPackage ./packages/clipboard-substitutor {};
+    diffuse-bin = callPackage ./packages/diffuse-bin {};
+    gdrive = callPackage ./packages/gdrive {};
+    gitice = callPackage ./packages/gitice {
+      inputs = { inherit fenix rust-manifest; };
+    };
+    gphotos-cdp = callPackage ./packages/gphotos-cdp {};
+    hcctl = callPackage ./packages/hcctl {};
+    healthchecks-monitor = callPackage ./packages/healthchecks-monitor {};
+    katbin = callPackage ./packages/katbin {};
+    linkleaner = callPackage ./packages/linkleaner {
+      inputs = { inherit fenix rust-manifest; };
+    };
+    monocraft-nerdfonts = callPackage ./packages/monocraft-nerdfonts {};
+    patreon-dl = callPackage ./packages/patreon-dl {};
+    pidcat = callPackage ./packages/pidcat {};
+    rucksack = callPackage ./packages/rucksack {};
+    tea-dev = callPackage ./packages/tea-dev {};
+    when = callPackage ./packages/when {};
+  }
---
+++ update.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+declare -a ALL_PACKAGES=(
+  adb-sync
+  adbtuifm
+  adx
+  clipboard-substitutor
+  gdrive
+  gitice
+  hcctl
+  healthchecks-monitor
+  katbin
+  linkleaner
+  monocraft-nerdfonts
+  patreon-dl
+  pidcat
+  rucksack
+  tea-dev
+  when
+)
+
+declare -A VERSION_REGEX=(
+  ["hcctl"]="hcctl-v(.*)"
+  ["healthchecks-monitor"]="healthchecks-monitor-v(.*)"
+)
+
+PKG="${1-}"
+VERSION="${2-}"
+NO_BUILD="${NO_BUILD-}"
+declare -a PACKAGES_TO_BUILD=()
+declare -a BASE_PARAMS=("--commit")
+if [ -z "${NO_BUILD}" ]; then
+  BASE_PARAMS+=("--build")
+fi
+
+if [ -z "${PKG}" ]; then
+  PACKAGES_TO_BUILD+=("${ALL_PACKAGES[@]}")
+else
+  PACKAGES_TO_BUILD+=("${PKG}")
+fi
+
+for PACKAGE in "${PACKAGES_TO_BUILD[@]}"; do
+  declare -a PARAMS=("${BASE_PARAMS[@]}")
+  if [[ -v VERSION_REGEX["${PACKAGE}"] ]]; then
+    PARAMS+=("--version-regex")
+    PARAMS+=("${VERSION_REGEX["${PACKAGE}"]}")
+  fi
+  if [ -n "${VERSION}" ]; then
+    PARAMS+=("--version")
+    PARAMS+=("${VERSION}")
+  fi
+  PARAMS+=("${PACKAGE}")
+  nix-update "${PARAMS[@]}"
+done
