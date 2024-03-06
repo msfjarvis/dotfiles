a38e78ed6c1bc19a1b19fa5c4c156d367e3efe59
Harsh Shandilya <me@msfjarvis.dev>
2024-03-07 02:23:57 +0530

    modules/darwin/nix: sync with NixOS module

--- modules/darwin/nix/default.nix
+++ modules/darwin/nix/default.nix
@@ -1,17 +1,58 @@
-_: {
+{
+  lib,
+  pkgs,
+  config,
+  inputs,
+  ...
+}: {
   nix = {
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 3d";
+    };
+
+    package = pkgs.nixUnstable;
+
+    registry = lib.mapAttrs (_: v: {flake = v;}) inputs;
+
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    extraOptions = ''
+      keep-outputs = true
+      warn-dirty = false
+      keep-derivations = true
+    '';
     settings = {
+      accept-flake-config = true;
+      allowed-users = ["msfjarvis"];
+      auto-optimise-store = true;
+      builders-use-substitutes = true;
+      experimental-features = [
+        "auto-allocate-uids"
+        "ca-derivations"
+        "cgroups"
+        "flakes"
+        "nix-command"
+        "recursive-nix"
+        "repl-flake"
+      ];
+      flake-registry = "/etc/nix/registry.json";
+      http-connections = 50;
+      keep-going = true;
+      log-lines = 20;
+      max-jobs = "auto";
+      sandbox = true;
+      trusted-users = ["root" "msfjarvis"];
+      warn-dirty = false;
+
       trusted-substituters = [
-        "https://cache.nixos.org/"
-        "https://cache.garnix.io/"
-        "https://nix-community.cachix.org/"
+        "https://cache.garnix.io"
+        "https://msfjarvis.cachix.org"
       ];
       trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "msfjarvis.cachix.org-1:/sKPgZblk/LgoOKtDgMTwvRuethILGkr/maOvZ6W11U="
       ];
-      trusted-users = ["msfjarvis" "root"];
     };
   };
   services.nix-daemon.enable = true;
