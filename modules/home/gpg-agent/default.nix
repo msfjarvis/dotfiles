51277f68dbf03261da039f5e53e74213df446dc8
Harsh Shandilya <me@msfjarvis.dev>
2024-03-13 13:20:01 +0530

    modules/home: manually set pinentry
    The home-manager way has been deprecated and now fails

--- modules/home/gpg-agent/default.nix
+++ modules/home/gpg-agent/default.nix
@@ -1,8 +1,15 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   services.gpg-agent = {
     enable = !pkgs.stdenv.isDarwin;
     defaultCacheTtl = 3600;
-    pinentryFlavor = "gnome3";
+    pinentryFlavor = null;
     enableBashIntegration = true;
+    extraConfig = ''
+      pinentry-program ${lib.getExe pkgs.pinentry-gnome3}
+    '';
   };
 }
