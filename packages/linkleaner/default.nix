813661ab8d4450507f07817ef1d4a824bf47033e
Harsh Shandilya <me@msfjarvis.dev>
2024-03-19 22:49:48 +0530

    linkleaner: 1.8.3 -> 1.9.0
    Diff: https://github.com/msfjarvis/linkleaner/compare/v1.8.3...v1.9.0

--- packages/linkleaner/default.nix
+++ packages/linkleaner/default.nix
@@ -15,19 +15,26 @@ in
   })
   .buildRustPackage rec {
     pname = "linkleaner";
-    version = "1.8.3";
+    version = "1.9.0";
 
     src = fetchFromGitHub {
       owner = "msfjarvis";
       repo = "linkleaner";
       rev = "v${version}";
-      hash = "sha256-lxKMmSySY5EbnK7GccoFBcQGaqkuir95nVOJkd4L08s=";
+      hash = "sha256-CagMcRpjyMq0/a7pKMCESa0NzuwskCTHWwtSvFtO1uM=";
     };
 
     buildInputs = lib.optionals stdenv.isDarwin [
       darwin.apple_sdk.frameworks.Security
       darwin.apple_sdk.frameworks.SystemConfiguration
     ];
+
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+      outputHashes = {
+        "teloxide-0.12.2" = "sha256-4GRyFlDQIdm3EgjLgTh1oVZ8X5Vyes4tigvKIb7UO5c=";
+      };
+    };
 
     cargoHash = "sha256-9oAC+B3uboNyIvk/C5WLlPZT/d4FgSKrGIMw40cbxEE=";
 
