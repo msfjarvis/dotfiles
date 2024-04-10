bd4ebcfc0c57840c7f2f50c4db38d7e70db84c36
Harsh Shandilya <me@msfjarvis.dev>
2024-04-10 14:54:43 +0530

    wailord: enable x86_64 emulation

--- systems/aarch64-linux/wailord/hardware-configuration.nix
+++ systems/aarch64-linux/wailord/hardware-configuration.nix
@@ -1,5 +1,6 @@
 {modulesPath, ...}: {
   imports = [(modulesPath + "/profiles/qemu-guest.nix")];
+  boot.binfmt.emulatedSystems = ["i686-linux" "x86_64-linux"];
   boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront"];
   boot.initrd.kernelModules = ["nvme"];
   disko.devices = {
