272a546a1117446055818e45db089af2f641a286
Harsh Shandilya <me@msfjarvis.dev>
2024-04-11 20:30:10 +0530

    nixos/desktop: fix noise cancelation profile

--- modules/nixos/profiles/desktop/noise-cancelation.nix
+++ modules/nixos/profiles/desktop/noise-cancelation.nix
@@ -5,44 +5,44 @@
   ...
 }: let
   cfg = config.profiles.desktop;
-  pw_rnnoise_config = {
-    "context.modules" = [
-      {
-        "name" = "libpipewire-module-filter-chain";
-        "args" = {
-          "node.description" = "Noise Canceling source";
-          "media.name" = "Noise Canceling source";
-          "filter.graph" = {
-            "nodes" = [
-              {
-                "type" = "ladspa";
-                "name" = "rnnoise";
-                "plugin" = "${pkgs.jarvis.rnnoise-plugin-slim}/lib/ladspa/librnnoise_ladspa.so";
-                "label" = "noise_suppressor_stereo";
-                "control" = {
-                  "VAD Threshold (%)" = 50.0;
-                };
-              }
-            ];
-          };
-          "audio.position" = ["FL" "FR"];
-          "capture.props" = {
-            "node.name" = "effect_input.rnnoise";
-            "node.passive" = true;
-          };
-          "playback.props" = {
-            "node.name" = "effect_output.rnnoise";
-            "media.class" = "Audio/Source";
-          };
-        };
-      }
-    ];
-  };
+  noise-suppression-for-voice = pkgs.writeTextDir "share/pipewire/pipewire.conf.d/99-noise-cancellation.conf" ''
+    context.modules = [
+    {   name = libpipewire-module-filter-chain
+        args = {
+            node.description =  "Noise Canceling source"
+            media.name =  "Noise Canceling source"
+            filter.graph = {
+                nodes = [
+                    {
+                        type = ladspa
+                        name = rnnoise
+                        plugin = ${pkgs.jarvis.rnnoise-plugin-slim}/lib/ladspa/librnnoise_ladspa.so
+                        label = noise_suppressor_mono
+                        control = {
+                            "VAD Threshold (%)" = 50.0
+                        }
+                    }
+                ]
+            }
+            capture.props = {
+                node.name =  "effect_input.rnnoise"
+                node.passive = true
+                audio.rate = 48000
+            }
+            playback.props = {
+                node.name =  "effect_output.rnnoise"
+                media.class = Audio/Source
+                audio.rate = 48000
+            }
+        }
+    }
+    ]
+  '';
 in {
   options.profiles.desktop.noise-cancelation = with lib; {
     enable = mkEnableOption "Enable noise cancelation in PipeWire";
   };
   config = lib.mkIf cfg.noise-cancelation.enable {
-    services.pipewire.extraConfig.pipewire = pw_rnnoise_config;
+    services.pipewire.configPackages = [noise-suppression-for-voice];
   };
 }
