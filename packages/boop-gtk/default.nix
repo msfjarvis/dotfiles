17135896ec53f86005f322a1aef7e528c9d67d17
Harsh Shandilya <me@msfjarvis.dev>
2024-04-18 16:55:49 +0530

    boop-gtk: add desktop item

--- packages/boop-gtk/default.nix
+++ packages/boop-gtk/default.nix
@@ -1,8 +1,12 @@
 {
   lib,
+  rust,
   rustPlatform,
+  stdenv,
   callPackage,
+  copyDesktopItems,
   fetchFromGitHub,
+  makeDesktopItem,
   pkg-config,
   wrapGAppsHook,
   atk,
@@ -32,6 +36,7 @@ rustPlatform.buildRustPackage rec {
   RUSTY_V8_ARCHIVE = callPackage ./librusty_v8.nix {};
 
   nativeBuildInputs = [
+    copyDesktopItems
     glib
     pkg-config
     wrapGAppsHook
@@ -40,6 +45,38 @@ rustPlatform.buildRustPackage rec {
   buildInputs = [atk cairo gdk-pixbuf gtk3 gtksourceview3 harfbuzz pango zlib];
 
   doCheck = false;
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "fyi.zoey.Boop-GTK";
+      exec = "boop-gtk";
+      icon = pname;
+      comment = meta.description;
+      desktopName = "Boop";
+      genericName = "Scriptable scratchpad";
+      categories = [
+        "Utility"
+        "Development"
+      ];
+      keywords = [
+        "Text"
+        "Editor"
+      ];
+      terminal = false;
+      type = "Application";
+      mimeTypes = [
+      ];
+    })
+  ];
+
+  installPhase = let
+    arch = rust.toRustTarget stdenv.hostPlatform;
+  in ''
+    runHook preInstall
+    install -Dm755 target/${arch}/release/boop-gtk $out/bin/boop-gtk
+    install -Dm644 data/fyi.zoey.Boop-GTK.png $out/share/icons/hicolor/64x64/${pname}.png
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description = "Port of @IvanMathy's Boop to GTK, a scriptable scratchpad for developers";
