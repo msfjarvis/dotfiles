0963aed41b49aae8d7f217332ce1bcffab2fbcd7
Harsh Shandilya <me@msfjarvis.dev>
2024-04-20 23:14:33 +0530

    packages: add nix-inspect

---
+++ packages/nix-inspect-worker/default.nix
@@ -0,0 +1,46 @@
+{
+  pkgs,
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+stdenv.mkDerivation {
+  pname = "nix-inspect";
+  version = "unstable-2024-04-19";
+
+  src = fetchFromGitHub {
+    owner = "bluskript";
+    repo = "nix-inspect";
+    rev = "86f636b1e01579b3a63b2c778c21a818b00c3d1e";
+    hash = "sha256-G5Md4ghux4LBRkPE8vzLTUWxzlQ7s1xKxZ8i3ICWZU8=";
+  };
+
+  nativeBuildInputs = with pkgs; [
+    meson
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = with pkgs; [
+    boost
+    nlohmann_json
+    nixVersions.nix_2_19.dev
+  ];
+
+  configurePhase = "pushd worker; meson setup build; popd";
+  buildPhase = "pushd worker; ninja -C build; popd";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp worker/build/nix-inspect $out/bin/
+  '';
+
+  meta = with lib; {
+    description = "Interactive tui for inspecting nix configs (Nix worker)";
+    homepage = "https://github.com/bluskript/nix-inspect";
+    changelog = "https://github.com/bluskript/nix-inspect/blob/${src.rev}/CHANGELOG.md";
+    license = licenses.mit;
+    maintainers = with maintainers; [msfjarvis];
+    mainProgram = "nix-inspect";
+  };
+}
---
+++ packages/nix-inspect/default.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "nix-inspect";
+  version = "unstable-2024-04-19";
+
+  src = fetchFromGitHub {
+    owner = "bluskript";
+    repo = "nix-inspect";
+    rev = "86f636b1e01579b3a63b2c778c21a818b00c3d1e";
+    hash = "sha256-G5Md4ghux4LBRkPE8vzLTUWxzlQ7s1xKxZ8i3ICWZU8=";
+  };
+
+  WORKER_BINARY_PATH = "${lib.getExe pkgs.jarvis.nix-inspect-worker}";
+
+  cargoHash = "sha256-go+av6Ia3/Jat4Nd9+8JkgENk2zEhmPfKjwjunBn0HU=";
+
+  meta = with lib; {
+    description = "Interactive tui for inspecting nix configs";
+    homepage = "https://github.com/bluskript/nix-inspect";
+    changelog = "https://github.com/bluskript/nix-inspect/blob/${src.rev}/CHANGELOG.md";
+    license = licenses.mit;
+    maintainers = with maintainers; [msfjarvis];
+    mainProgram = "nix-inspect";
+  };
+}
