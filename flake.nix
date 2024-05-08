4342a893d4e0d8301cb0476fbc2334f4c926aefd
Harsh Shandilya <me@msfjarvis.dev>
2024-05-08 13:50:47 +0530

    nix: use stylix GNOME 46 branch

--- flake.nix
+++ flake.nix
@@ -209,7 +209,7 @@
     srvos.url = "github:nix-community/srvos";
     srvos.inputs.nixpkgs.follows = "nixpkgs";
 
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:danth/stylix/refs/pull/357/merge";
     stylix.inputs.flake-compat.follows = "flake-compat";
     stylix.inputs.home-manager.follows = "home-manager";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
