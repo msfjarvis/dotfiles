15daf6f7a5708979ccffb19aa936838b82d8275a
Harsh Shandilya <me@msfjarvis.dev>
2024-05-26 12:47:17 +0530

    spotify: set correct color scheme

--- modules/home/spotify/default.nix
+++ modules/home/spotify/default.nix
@@ -18,6 +18,7 @@ in
     programs.spicetify = {
       enable = true;
       theme = spicePkgs.themes.catppuccin;
+      colorScheme = "mocha";
 
       enabledCustomApps = with spicePkgs.apps; [ lyricsPlus ];
 
