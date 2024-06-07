6bab1cdda0247d280ddb3a478c6a315535d264b7
Harsh Shandilya <me@msfjarvis.dev>
2024-06-08 03:42:57 +0530

    nixos/betula: init
    Fixes PP-93

---
+++ modules/nixos/betula/default.nix
@@ -0,0 +1,79 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.services.betula;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    mkPackageOptionMD
+    types
+    ;
+in
+{
+  options.services.betula = {
+    enable = mkEnableOption { description = "Whether to enable the betula bookmarking service."; };
+
+    dataDir = mkOption {
+      type = types.str;
+      default = "/var/lib/betula/";
+      description = "Path to the directory where betula will persist its state.";
+    };
+
+    user = mkOption {
+      type = types.str;
+      default = "betula";
+      description = "User account under which betula runs.";
+    };
+
+    group = mkOption {
+      type = types.str;
+      default = "betula";
+      description = "Group account under which betula runs.";
+    };
+
+    package = mkPackageOptionMD pkgs "betula" { };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.betula = {
+      wantedBy = [ "default.target" ];
+      after = [
+        "fs.service"
+        "networking.target"
+      ];
+      wants = [
+        "fs.service"
+        "networking.target"
+      ];
+
+      serviceConfig = {
+        User = cfg.user;
+        Group = cfg.group;
+        Restart = "on-failure";
+        RestartSec = "30s";
+        Type = "oneshot";
+      };
+      script = ''
+        ${lib.getExe cfg.package} ${cfg.dataDir}/sqlite.db
+      '';
+    };
+
+    systemd.tmpfiles.rules = [ "d ${cfg.dataDir} 0755 ${cfg.user} ${cfg.group} - -" ];
+
+    users.users = mkIf (cfg.user == "betula") {
+      betula = {
+        inherit (cfg) group;
+        createHome = false;
+        description = "betula daemon user";
+        isNormalUser = true;
+      };
+    };
+
+    users.groups = mkIf (cfg.group == "betula") { betula = { }; };
+  };
+}
