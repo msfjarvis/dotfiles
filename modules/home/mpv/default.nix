0581bfc025937350dad5be1b7c3007555c7cff39
Harsh Shandilya <me@msfjarvis.dev>
2024-06-16 16:25:04 +0530

    mpv: adapt for https://github.com/NixOS/nixpkgs/pull/304349

--- modules/home/mpv/default.nix
+++ modules/home/mpv/default.nix
@@ -15,48 +15,47 @@ in
   config = mkIf cfg.enable {
     programs.mpv = {
       enable = true;
-      package =
-        pkgs.wrapMpv
-          (pkgs.mpv-unwrapped.override {
-            waylandSupport = true;
-            x11Support = false;
-            cddaSupport = false;
-            vulkanSupport = false;
-            drmSupport = false;
-            archiveSupport = false;
-            bluraySupport = false;
-            bs2bSupport = false;
-            cacaSupport = false;
-            cmsSupport = false;
-            dvdnavSupport = false;
-            dvbinSupport = false;
-            jackaudioSupport = false;
-            javascriptSupport = false;
-            libpngSupport = false;
-            openalSupport = false;
-            pulseSupport = false;
-            pipewireSupport = true;
-            rubberbandSupport = false;
-            screenSaverSupport = false;
-            sdl2Support = true;
-            sixelSupport = false;
-            speexSupport = false;
-            swiftSupport = false;
-            theoraSupport = false;
-            vaapiSupport = true;
-            vapoursynthSupport = false;
-            vdpauSupport = true;
-            xineramaSupport = false;
-            xvSupport = false;
-            zimgSupport = false;
-          })
-          {
-            scripts = with pkgs.mpvScripts; [
-              uosc
-              thumbfast
-              inhibit-gnome
-            ];
-          };
+      package = pkgs.mpv-unwrapped.wrapper {
+        mpv = pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+          x11Support = false;
+          cddaSupport = false;
+          vulkanSupport = false;
+          drmSupport = false;
+          archiveSupport = false;
+          bluraySupport = false;
+          bs2bSupport = false;
+          cacaSupport = false;
+          cmsSupport = false;
+          dvdnavSupport = false;
+          dvbinSupport = false;
+          jackaudioSupport = false;
+          javascriptSupport = false;
+          libpngSupport = false;
+          openalSupport = false;
+          pulseSupport = false;
+          pipewireSupport = true;
+          rubberbandSupport = false;
+          screenSaverSupport = false;
+          sdl2Support = true;
+          sixelSupport = false;
+          speexSupport = false;
+          swiftSupport = false;
+          theoraSupport = false;
+          vaapiSupport = true;
+          vapoursynthSupport = false;
+          vdpauSupport = true;
+          xineramaSupport = false;
+          xvSupport = false;
+          zimgSupport = false;
+        };
+        scripts = with pkgs.mpvScripts; [
+          uosc
+          thumbfast
+          inhibit-gnome
+        ];
+
+      };
       config = {
         autofit = "100%";
         window-maximized = "yes";
