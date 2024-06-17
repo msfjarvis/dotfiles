a7a47234a86f723ea3f9ebca627d115040a580f3
Harsh Shandilya <me@msfjarvis.dev>
2024-06-18 01:19:14 +0530

    ryzenbox: re-enable Logseq

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -29,7 +29,7 @@ _final: prev: {
   logseq = prev.logseq.overrideAttrs (old: {
     # Remove NIXOS_OZONE_WL compat which breaks the app entirely
     postFixup = ''
-      makeWrapper ${prev.electron_27}/bin/electron $out/bin/${old.pname} \
+      makeWrapper ${prev.lib.getExe prev.electron} $out/bin/${old.pname} \
         --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
         --add-flags $out/share/${old.pname}/resources/app
     '';
