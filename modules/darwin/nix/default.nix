c8550164a7711ac2767fddac745d0eb9d2049f8b
Harsh Shandilya <me@msfjarvis.dev>
2024-07-04 04:01:01 +0530

    darwin: use CppNix
    Lix tests fail on my Mac

--- modules/darwin/nix/default.nix
+++ modules/darwin/nix/default.nix
@@ -7,6 +7,7 @@
 }:
 {
   nix = lib.${namespace}.mkNixConfig { inherit lib pkgs inputs; } // {
+    package = pkgs.nixVersions.stable;
     gc = {
       automatic = true;
       options = "--delete-older-than 3d";
