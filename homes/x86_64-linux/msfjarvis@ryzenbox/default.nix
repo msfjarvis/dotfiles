bdd793b750b4010cf2990f7374f9e6d7ceab13e5
Harsh Shandilya <me@msfjarvis.dev>
2024-07-11 02:06:01 +0530

    zed: expand module

--- homes/x86_64-linux/msfjarvis@ryzenbox/default.nix
+++ homes/x86_64-linux/msfjarvis@ryzenbox/default.nix
@@ -1,4 +1,5 @@
-_: {
+{ pkgs, inputs, ... }:
+{
   snowfallorg.user = {
     enable = true;
     name = "msfjarvis";
@@ -8,7 +9,43 @@ _: {
   profiles.mpv.enable = true;
   profiles.pass.enable = true;
   profiles.spotify.enable = true;
-  profiles.zed.enable = true;
+  profiles.zed = {
+    enable = true;
+    extraPackages = with pkgs; [
+      # Nix
+      nixd
+      # Rust
+      inputs.fenix.packages.${system}.rust-analyzer
+    ];
+    extensions = [
+      "catppuccin"
+      "html"
+      "nix"
+      "toml"
+    ];
+    userSettings = {
+      theme = "Catppuccin Mocha - No Italics";
+      ui_font_size = 16;
+      buffer_font_size = 16;
+      show_wrap_guides = true;
+      indent_guides.enabled = true;
+      toolbar = {
+        breadcrumbs = true;
+        quick_actions = true;
+        selections_menu = true;
+      };
+      inlayHints = {
+        maxLength = null;
+        lifetimeElisionHints = {
+          useParameterNames = true;
+          enable = "skip_trivial";
+        };
+        closureReturnTypeHints = {
+          enable = true;
+        };
+      };
+    };
+  };
 
   home.stateVersion = "21.05";
 }
--- modules/home/zed/default.nix
+++ modules/home/zed/default.nix
@@ -1,25 +1,71 @@
 {
   config,
-  pkgs,
   lib,
-  inputs,
+  pkgs,
   ...
 }:
 let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    mkPackageOption
+    types
+    ;
   cfg = config.profiles.zed;
-  inherit (lib) mkEnableOption mkIf;
+  jsonFormat = pkgs.formats.json { };
+
+  mergedSettings = cfg.userSettings // {
+    # this part by @cmacrae
+    auto_install_extensions = lib.listToAttrs (map (ext: lib.nameValuePair ext true) cfg.extensions);
+  };
 in
 {
   options.profiles.zed = {
-    enable = mkEnableOption "Enable zed editor (zed.dev)";
+    enable = mkEnableOption "Zed, the high performance, multiplayer code editor from the creators of Atom and Tree-sitter";
+    package = mkPackageOption pkgs "zed-editor" { };
+
+    userSettings = mkOption {
+      inherit (jsonFormat) type;
+      default = { };
+      description = ''
+        Configuration written to Zed's {file}`settings.json`.
+      '';
+    };
+
+    userKeymaps = mkOption {
+      inherit (jsonFormat) type;
+      default = { };
+      description = ''
+        Configuration written to Zed's {file}`keymap.json`.
+      '';
+    };
+
+    extensions = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      description = ''
+        A list of the extensions Zed should install on startup.
+        Use the name of a repository in the [extension list](https://github.com/zed-industries/extensions/tree/main/extensions).
+      '';
+    };
+
+    extraPackages = mkOption {
+      type = types.listOf types.package;
+      default = [ ];
+      description = ''
+        A list of additional packages to install alongside Zed.
+      '';
+    };
   };
+
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      # Nix
-      nixd
-      # Rust
-      inputs.fenix.packages.${system}.rust-analyzer
-      zed-editor
-    ];
+    home.packages = [ cfg.package ] ++ cfg.extraPackages;
+    xdg.configFile."zed/settings.json" = mkIf (mergedSettings != { }) {
+      source = jsonFormat.generate "zed-user-settings" mergedSettings;
+    };
+    xdg.configFile."zed/keymap.json" = mkIf (cfg.userKeymaps != { }) {
+      source = jsonFormat.generate "zed-user-keymaps" cfg.userKeymaps;
+    };
   };
 }
