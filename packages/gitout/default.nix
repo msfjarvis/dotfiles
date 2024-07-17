msfjarvis/dotfiles

commit 3669d5da01edb0975a5f32d4da6bd64e0f72f7dc

refactor(packages): avoid `rec`
Fixes PP-100

packages/gitout/default.nix
@@ -10,9 +10,12 @@
   darwin,
   lib,
 }:
-rustPlatform.buildRustPackage rec {
-  pname = "gitout";
+let
   version = "0.2.2";
+in
+rustPlatform.buildRustPackage {
+  pname = "gitout";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "msfjarvis";

packages/katbin/default.nix
@@ -5,9 +5,12 @@
   stdenv,
   lib,
 }:
-rustPlatform.buildRustPackage rec {
-  pname = "katbin-cli";
+let
   version = "1.3.15";
+in
+rustPlatform.buildRustPackage {
+  pname = "katbin-cli";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "SphericalKat";

packages/nix-inspect/default.nix
@@ -4,7 +4,7 @@
   rustPlatform,
   fetchFromGitHub,
 }:
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage {
   pname = "nix-inspect";
   version = "unstable-2024-04-19";
 

packages/toml-cli/default.nix
@@ -3,9 +3,12 @@
   rustPlatform,
   fetchFromGitHub,
 }:
-rustPlatform.buildRustPackage rec {
-  pname = "toml-cli";
+let
   version = "0.2.3";
+in
+rustPlatform.buildRustPackage {
+  pname = "toml-cli";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "gnprice";

packages/when/default.nix
@@ -3,9 +3,12 @@
   rustPlatform,
   lib,
 }:
-rustPlatform.buildRustPackage rec {
-  pname = "when";
+let
   version = "0.4.0";
+in
+rustPlatform.buildRustPackage {
+  pname = "when";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "mitsuhiko";

packages/adbtuifm/default.nix
@@ -3,9 +3,12 @@
   fetchFromGitHub,
   lib,
 }:
-buildGoModule rec {
-  pname = "adbtuifm";
+let
   version = "0.5.8";
+in
+buildGoModule {
+  pname = "adbtuifm";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "darkhz";

packages/adx/default.nix
@@ -5,9 +5,12 @@
   stdenv,
   lib,
 }:
-rustPlatform.buildRustPackage rec {
-  pname = "adx";
+let
   version = "5.0.0";
+in
+rustPlatform.buildRustPackage {
+  pname = "adx";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "msfjarvis";

packages/boop-gtk/default.nix
@@ -19,9 +19,13 @@
   pango,
   zlib,
 }:
-rustPlatform.buildRustPackage rec {
+let
   pname = "boop-gtk";
   version = "1.9.0";
+  description = "Port of @IvanMathy's Boop to GTK, a scriptable scratchpad for developers";
+in
+rustPlatform.buildRustPackage {
+  inherit pname version;
 
   src = fetchFromGitHub {
     owner = "msfjarvis";
@@ -60,7 +64,7 @@ rustPlatform.buildRustPackage rec {
       name = "fyi.zoey.Boop-GTK";
       exec = "boop-gtk";
       icon = pname;
-      comment = meta.description;
+      comment = description;
       desktopName = "Boop";
       genericName = "Scriptable scratchpad";
       categories = [
@@ -84,11 +88,11 @@ rustPlatform.buildRustPackage rec {
     runHook postInstall
   '';
 
-  meta = with lib; {
-    description = "Port of @IvanMathy's Boop to GTK, a scriptable scratchpad for developers";
+  meta = {
+    inherit description;
     homepage = "https://github.com/msfjarvis/boop-gtk";
-    license = licenses.mit;
+    license = lib.licenses.mit;
     mainProgram = "boop-gtk";
-    platforms = platforms.linux;
+    platforms = lib.platforms.linux;
   };
 }

commit 93c6b1d1fa1d0cf36113eaa1914dc42691a5c432

packages: fix meta
Fixes: 3669d5da01ed ("refactor(packages): avoid `rec`")

packages/gitout/default.nix
@@ -42,7 +42,7 @@ rustPlatform.buildRustPackage {
   meta = with lib; {
     description = "A command-line tool and Docker image to automatically backup Git repositories from GitHub or anywhere";
     homepage = "https://github.com/msfjarvis/gitout";
-    changelog = "https://github.com/msfjarvis/gitout/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/msfjarvis/gitout/blob/${version}/CHANGELOG.md";
     license = licenses.mit;
     mainProgram = "gitout";
   };

packages/katbin/default.nix
@@ -28,7 +28,7 @@ rustPlatform.buildRustPackage {
   meta = with lib; {
     description = "A CLI for katbin";
     homepage = "https://github.com/SphericalKat/katbin-cli";
-    changelog = "https://github.com/SphericalKat/katbin-cli/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/SphericalKat/katbin-cli/blob/${version}/CHANGELOG.md";
     license = with licenses; [
       asl20
       mit

packages/nix-inspect/default.nix
@@ -22,7 +22,7 @@ rustPlatform.buildRustPackage {
   meta = with lib; {
     description = "Interactive tui for inspecting nix configs";
     homepage = "https://github.com/bluskript/nix-inspect";
-    changelog = "https://github.com/bluskript/nix-inspect/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/bluskript/nix-inspect/blob/${version}/CHANGELOG.md";
     license = licenses.mit;
     mainProgram = "nix-inspect";
   };

packages/toml-cli/default.nix
@@ -25,7 +25,7 @@ rustPlatform.buildRustPackage {
   meta = with lib; {
     description = "Simple CLI for editing and querying TOML files.";
     homepage = "https://github.com/gnprice/toml-cli";
-    changelog = "https://github.com/gnprice/toml-cli/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/gnprice/toml-cli/blob/${version}/CHANGELOG.md";
     license = licenses.mit;
     mainProgram = "toml-cli";
   };

packages/when/default.nix
@@ -25,7 +25,7 @@ rustPlatform.buildRustPackage {
   meta = with lib; {
     description = "Timezones from the command line";
     homepage = "https://github.com/mitsuhiko/when";
-    changelog = "https://github.com/mitsuhiko/when/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/mitsuhiko/when/blob/${version}/CHANGELOG.md";
     license = licenses.asl20;
     mainProgram = "when";
   };