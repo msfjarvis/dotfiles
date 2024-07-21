0e5790261c49a84f66c7901e3062e67aa027fd82
Harsh Shandilya <me@msfjarvis.dev>
2024-07-21 14:33:44 +0530

    fixup! ryzenbox: add Minecraft 1.21

--- systems/x86_64-linux/ryzenbox/default.nix
+++ systems/x86_64-linux/ryzenbox/default.nix
@@ -331,9 +331,9 @@
           (minecraft "ACLU SMP Modpack")
           (minecraft "Fabulously.Optimized.1.20.2")
           (minecraft "Fabulously.Optimized.1.20.6")
+          (minecraft "Fabulously.Optimized.1.21")
           (minecraft "Fabulously.Optimized.MC.1.17.1")
           (minecraft "Fabulously.Optimized.MC.1.20.1")
-          (minecraft "Fabulously.Optimized.MC.1.21")
           (minecraft "Pokemon Elysium")
           "${home}/Games/PrismLauncher/instances/Vault Hunters 3/minecraft/screenshots/"
         ];
