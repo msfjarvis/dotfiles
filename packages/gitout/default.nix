d86584e3692665f60ee8c97a0e055c38c785c52a
Harsh Shandilya <me@msfjarvis.dev>
2024-08-05 00:19:23 +0530

    gitout: fix Darwin build

--- packages/gitout/default.nix
+++ packages/gitout/default.nix
@@ -33,16 +33,24 @@ rustPlatform.buildRustPackage {
     pkg-config
   ];
 
-  buildInputs = [
-    libgit2
-    openssl
-    zlib
-  ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+  buildInputs =
+    [
+      libgit2
+      openssl
+      zlib
+    ]
+    ++ lib.optionals stdenv.isDarwin (
+      with darwin.apple_sdk.frameworks;
+      [
+        Security
+        SystemConfiguration
+      ]
+    );
 
   meta = with lib; {
     description = "A command-line tool and Docker image to automatically backup Git repositories from GitHub or anywhere";
     homepage = "https://github.com/msfjarvis/gitout";
-    changelog = "https://github.com/msfjarvis/gitout/blob/${version}/CHANGELOG.md";
+    changelog = "https://github.com/msfjarvis/gitout/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     mainProgram = "gitout";
   };
