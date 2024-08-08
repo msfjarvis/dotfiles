9b141f5c621c439a79500be6a01ac8511c33f336
Harsh Shandilya <me@msfjarvis.dev>
2024-08-08 13:25:55 +0530

    noise-cancelation: inline package definition

--- modules/nixos/profiles/desktop/noise-cancelation.nix
+++ modules/nixos/profiles/desktop/noise-cancelation.nix
@@ -8,47 +8,47 @@
 let
   cfg = config.profiles.${namespace}.desktop;
   inherit (lib) mkEnableOption mkIf;
-  noise-suppression-for-voice = pkgs.writeTextDir "share/pipewire/pipewire.conf.d/99-noise-cancellation.conf" ''
-    context.modules = [
-    {   name = libpipewire-module-filter-chain
-        args = {
-            node.description =  "Noise Canceling source"
-            media.name =  "Noise Canceling source"
-            filter.graph = {
-                nodes = [
-                    {
-                        type = ladspa
-                        name = rnnoise
-                        plugin = ${
-                          pkgs.${namespace}.rnnoise-plugin-slim
-                        }/lib/ladspa/librnnoise_ladspa.so
-                        label = noise_suppressor_mono
-                        control = {
-                            "VAD Threshold (%)" = 50.0
-                        }
-                    }
-                ]
-            }
-            capture.props = {
-                node.name =  "effect_input.rnnoise"
-                node.passive = true
-                audio.rate = 48000
-            }
-            playback.props = {
-                node.name =  "effect_output.rnnoise"
-                media.class = Audio/Source
-                audio.rate = 48000
-            }
-        }
-    }
-    ]
-  '';
 in
 {
   options.profiles.${namespace}.desktop.noise-cancelation = {
     enable = mkEnableOption "Enable noise cancelation in PipeWire";
   };
   config = mkIf cfg.noise-cancelation.enable {
-    services.pipewire.configPackages = [ noise-suppression-for-voice ];
+    services.pipewire.configPackages = [
+      (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/49-noise-cancellation.conf" ''
+        context.modules = [
+          {
+            name = libpipewire-module-filter-chain
+            args = {
+              node.description =  "Noise Canceling source"
+              media.name =  "Noise Canceling source"
+              filter.graph = {
+                nodes = [
+                  {
+                    type = ladspa
+                    name = rnnoise
+                    plugin = ${pkgs.${namespace}.rnnoise-plugin-slim}/lib/ladspa/librnnoise_ladspa.so
+                    label = noise_suppressor_mono
+                    control = {
+                      "VAD Threshold (%)" = 50.0
+                    }
+                  }
+                ]
+              }
+              capture.props = {
+                node.name =  "effect_input.rnnoise"
+                node.passive = true
+                audio.rate = 48000
+              }
+              playback.props = {
+                node.name =  "effect_output.rnnoise"
+                media.class = Audio/Source
+                audio.rate = 48000
+              }
+            }
+          }
+        ]
+      '')
+    ];
   };
 }
