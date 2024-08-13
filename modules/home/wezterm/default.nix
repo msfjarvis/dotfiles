42e45a2b081db0893923f5ec120f88428c4d306c
Harsh Shandilya <me@msfjarvis.dev>
2024-08-13 12:22:42 +0530

    wezterm: add window decor tweaks

--- modules/home/wezterm/default.nix
+++ modules/home/wezterm/default.nix
@@ -36,6 +36,37 @@ in
         config.initial_cols = 169
         config.initial_rows = 42
 
+        -- Tweak Window decorations
+        config.window_background_opacity = 0.95
+        config.window_decorations = 'RESIZE'
+        config.window_frame = {
+          font = wezterm.font 'JetBrains Mono Nerd Font Mono',
+          font_size = 11.0
+        }
+        wezterm.on('update-status', function(window)
+          -- Grab the utf8 character for the "powerline" left facing
+          -- solid arrow.
+          local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+          -- Grab the current window configuration, and from it the
+          -- palette (this is the combination of your chosen colour scheme
+          -- including any overrides).
+          local color_scheme = window:effective_config().resolved_palette
+          local bg = color_scheme.background
+          local fg = color_scheme.foreground
+
+          window:set_right_status(wezterm.format({
+            -- First, we draw the arrow...
+            { Background = { Color = 'none' } },
+            { Foreground = { Color = bg } },
+            { Text = SOLID_LEFT_ARROW },
+            -- Then we draw our text
+            { Background = { Color = bg } },
+            { Foreground = { Color = fg } },
+            { Text = ' ' .. wezterm.hostname() .. ' ' },
+          }))
+        end)
+
         return config
       '';
     };
