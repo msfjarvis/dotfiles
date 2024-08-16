0c19ad7b2654ac65c87810f8eba128452af00308
Harsh Shandilya <me@msfjarvis.dev>
2024-08-16 23:41:38 +0530

    gnome-terminal: set up a default profile

--- modules/home/gnome-terminal/default.nix
+++ modules/home/gnome-terminal/default.nix
@@ -14,13 +14,37 @@ in
     enable = mkEnableOption "GNOME Terminal";
   };
   config = mkIf cfg.enable {
-    programs.gnome-terminal.enable = true;
+    programs.gnome-terminal = {
+      enable = true;
+      profile = {
+        "b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
+          default = true;
+          visibleName = "Catppuccin Mocha";
+          colors = with config.lib.stylix.colors.withHashtag; {
+            foregroundColor = base05;
+            backgroundColor = base00;
+            boldColor = base04;
+            palette = [
+              base00
+              base08
+              base0B
+              base0A
+              base0D
+              base0E
+              base0C
+              base05
+            ];
+          };
+          boldIsBright = true;
+          audibleBell = false;
+        };
+      };
+    };
     dconf.settings = {
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = lib.mkForce {
         binding = "<Control><Alt>t";
         command = "${lib.getExe pkgs.gnome-terminal}";
       };
-
     };
   };
 }
