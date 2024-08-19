71756a1ab3db680f19da912c0b4310d24bc8c3d2
Harsh Shandilya <me@msfjarvis.dev>
2024-08-20 00:23:27 +0530

    templates/go: remove VSCode

--- templates/go/flake.nix
+++ templates/go/flake.nix
@@ -14,17 +14,11 @@
   inputs.flake-compat.url = "github:nix-community/flake-compat";
   inputs.flake-compat.flake = false;
 
-  inputs.nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
-  inputs.nix-vscode-extensions.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nix-vscode-extensions.inputs.flake-utils.follows = "flake-utils";
-  inputs.nix-vscode-extensions.inputs.flake-compat.follows = "flake-compat";
-
   outputs =
     {
       nixpkgs,
       devshell,
       flake-utils,
-      nix-vscode-extensions,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -33,10 +27,7 @@
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
-          overlays = [
-            devshell.overlays.default
-            nix-vscode-extensions.overlays.default
-          ];
+          overlays = [ devshell.overlays.default ];
         };
       in
       {
@@ -58,17 +49,6 @@
             go_1_21
             gopls
             gotools
-            (vscode-with-extensions.override {
-              vscodeExtensions = with pkgs.vscode-marketplace; [
-                arrterian.nix-env-selector
-                eamodio.gitlens
-                golang.go
-                jnoortheen.nix-ide
-                k--kato.intellij-idea-keybindings
-                mtdmali.daybreak-theme
-                oderwat.indent-rainbow
-              ];
-            })
           ];
         };
       }
