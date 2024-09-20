e3537221cf9536f2180ccd50c21cce78ccfd943f
Harsh Shandilya <me@msfjarvis.dev>
2024-09-20 22:33:23 +0530

    nix: restore nixos-cosmic

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
       systems.modules.nixos = with inputs; [
         disko.nixosModules.disko
         nix-topology.nixosModules.default
-        # nixos-cosmic.nixosModules.default
+        nixos-cosmic.nixosModules.default
         sops-nix.nixosModules.sops
         stylix.nixosModules.stylix
         srvos.nixosModules.common
@@ -174,10 +174,16 @@
     nix-vscode-extensions.inputs.flake-utils.follows = "flake-utils";
     nix-vscode-extensions.inputs.flake-compat.follows = "flake-compat";
 
-    # nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
-    # nixos-cosmic.inputs.nixpkgs.follows = "nixpkgs";
-    # nixos-cosmic.inputs.nixpkgs-stable.follows = "nixpkgs";
-    # nixos-cosmic.inputs.flake-compat.follows = "flake-compat";
+    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
+    nixos-cosmic.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-cosmic.inputs.nixpkgs-stable.follows = "nixpkgs";
+    nixos-cosmic.inputs.flake-compat.follows = "flake-compat";
+    nixos-cosmic.inputs.nix-update.follows = "nix-update";
+
+    nix-update.url = "github:Mic92/nix-update";
+    nix-update.inputs.nixpkgs.follows = "nixpkgs";
+    nix-update.inputs.flake-parts.follows = "flake-parts";
+    nix-update.inputs.treefmt-nix.follows = "treefmt-nix";
 
     nixos-vscode-server.url = "github:nix-community/nixos-vscode-server";
     nixos-vscode-server.inputs.nixpkgs.follows = "nixpkgs";
