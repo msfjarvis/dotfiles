90b07086e2594622455a687b9a56de020ea3b8dd
Harsh Shandilya <me@msfjarvis.dev>
2024-10-01 12:53:22 +0530

    ryzenbox: use the same FFMPEG as Handbrake

--- systems/x86_64-linux/ryzenbox/default.nix
+++ systems/x86_64-linux/ryzenbox/default.nix
@@ -84,26 +84,7 @@
       pkgs.${namespace}.cyberdrop-dl
       dconf2nix
       fclones
-      (ffmpeg.override {
-        buildFfplay = false;
-        buildFfprobe = true;
-        buildQtFaststart = false;
-        withAom = true;
-        withAss = true;
-        withDav1d = true;
-        withDrm = true;
-        withMp3lame = true;
-        withNvdec = true;
-        withNvenc = true;
-        withRav1e = true;
-        withVaapi = true;
-        withVdpau = true;
-        withVorbis = true;
-        withVpx = true;
-        withWebp = true;
-        withX264 = true;
-        withX265 = true;
-      })
+      ffmpeg_7-full
       (inputs.firefox.packages.${pkgs.system}.firefox-nightly-bin.override {
         cfg = {
           smartcardSupport = true;
