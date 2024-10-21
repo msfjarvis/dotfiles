b0cb44821d752be14a8d3e8c0b8dc195f9ec0a1a
Harsh Shandilya <me@msfjarvis.dev>
2024-10-21 16:48:22 +0530

    gaming: add ficsit-cli

--- modules/nixos/profiles/desktop/gaming.nix
+++ modules/nixos/profiles/desktop/gaming.nix
@@ -150,6 +150,7 @@ in
           }
         ];
       })
+      pkgs.${namespace}.ficsit-cli
     ];
     # Required to avoid some logspew
     environment.sessionVariables.VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
