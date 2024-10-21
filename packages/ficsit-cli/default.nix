8534710f3a9e587abf9873328b87e063123de218
Harsh Shandilya <me@msfjarvis.dev>
2024-10-21 16:47:25 +0530

    ficsit-cli: init at 0.6.0

---
+++ packages/ficsit-cli/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule rec {
+  pname = "ficsit-cli";
+  version = "0.6.0";
+
+  src = fetchFromGitHub {
+    owner = "satisfactorymodding";
+    repo = "ficsit-cli";
+    rev = "v${version}";
+    hash = "sha256-Zwidx0war3hos9NEmk9dEzPBgDGdUtWvZb7FIF5OZMA=";
+  };
+
+  vendorHash = "sha256-vmA3jvxOLRYj5BmvWMhSEnCTEoe8BLm8lpm2kruIEv4=";
+
+  ldflags = [ "-s" "-w" ];
+
+  # Tests try to access the live ficsit.app API
+  doCheck = false;
+
+  meta = {
+    description = "A CLI tool for managing mods for the game Satisfactory";
+    homepage = "https://github.com/satisfactorymodding/ficsit-cli";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "ficsit-cli";
+  };
+}
