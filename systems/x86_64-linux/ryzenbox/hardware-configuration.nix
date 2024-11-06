09baa58864f58990f3bf0f93c457cb7cd9dd4032
Harsh Shandilya <me@msfjarvis.dev>
2024-11-07 01:38:35 +0530

    ryzenbox: strip down hardware configuration
    Let nixos-facter deal with this

--- systems/x86_64-linux/ryzenbox/hardware-configuration.nix
+++ systems/x86_64-linux/ryzenbox/hardware-configuration.nix
@@ -4,30 +4,15 @@
 {
   config,
   lib,
-  modulesPath,
   ...
 }:
 let
   nvidiaDriver = config.boot.kernelPackages.nvidiaPackages.beta.bin;
 in
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
-
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-  boot.initrd.availableKernelModules = [
-    "nvme"
-    "xhci_pci"
-    "ahci"
-    "usb_storage"
-    "usbhid"
-    "sd_mod"
-  ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
-  boot.supportedFilesystems = [ "ntfs" ];
 
-  # Enable OpenGL
+  # Enable NVIDIA driver
   hardware.graphics = {
     enable = true;
     package = nvidiaDriver;
@@ -105,16 +90,5 @@ in
     fsType = "ext4";
   };
 
-  swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp12s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp13s0.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
