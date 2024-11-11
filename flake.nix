ea793f8ef0ac8b2982f16f99642b59775afda5f7
Harsh Shandilya <me@msfjarvis.dev>
2024-11-11 14:48:10 +0530

    flake.lock: Update
    Flake lock file updates:

--- flake.nix
+++ flake.nix
@@ -82,7 +82,8 @@
 
   inputs = {
     # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:msfjarvis/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "github:msfjarvis/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:msfjarvis/nixpkgs/stable-pin";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     systems.url = "github:msfjarvis/flake-systems";
