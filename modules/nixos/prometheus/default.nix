3ef6c1ac4dbaa6714d5a2c198d3293e2dbe77a36
Harsh Shandilya <me@msfjarvis.dev>
2024-11-11 15:16:07 +0530

    prometheus: set up Loki and Promtail

--- modules/nixos/prometheus/default.nix
+++ modules/nixos/prometheus/default.nix
@@ -49,6 +49,65 @@ in
         };
       };
     };
+    services.loki = {
+      enable = true;
+      configuration = {
+        # Basic stuff
+        auth_enabled = false;
+        server = {
+          http_listen_port = 3100;
+          log_level = "warn";
+        };
+        common = {
+          path_prefix = config.services.loki.dataDir;
+          storage.filesystem = {
+            chunks_directory = "${config.services.loki.dataDir}/chunks";
+            rules_directory = "${config.services.loki.dataDir}/rules";
+          };
+          replication_factor = 1;
+          ring.kvstore.store = "inmemory";
+          ring.instance_addr = "127.0.0.1";
+        };
+
+        ingester.chunk_encoding = "snappy";
+
+        limits_config = {
+          retention_period = "120h";
+          ingestion_burst_size_mb = 16;
+          reject_old_samples = true;
+          reject_old_samples_max_age = "12h";
+        };
+
+        table_manager = {
+          retention_deletes_enabled = true;
+          retention_period = "120h";
+        };
+
+        compactor = {
+          retention_enabled = true;
+          compaction_interval = "10m";
+          working_directory = "${config.services.loki.dataDir}/compactor";
+          delete_request_cancel_period = "10m"; # don't wait 24h before processing the delete_request
+          retention_delete_delay = "2h";
+          retention_delete_worker_count = 150;
+          delete_request_store = "filesystem";
+        };
+
+        schema_config.configs = [
+          {
+            from = "2020-11-08";
+            store = "tsdb";
+            object_store = "filesystem";
+            schema = "v13";
+            index.prefix = "index_";
+            index.period = "24h";
+          }
+        ];
+
+        query_range.cache_results = true;
+        limits_config.split_queries_by_interval = "24h";
+      };
+    };
     services.prometheus = {
       enable = true;
       port = 9001;
@@ -81,6 +140,53 @@ in
           static_configs = [ { targets = [ config.services.miniflux.config.LISTEN_ADDR ]; } ];
         }
       ];
+    };
+    services.promtail = {
+      enable = false; # Haven't figured out how to allow it to read Caddy logs
+      configuration = {
+        server = {
+          http_listen_port = 3031;
+          grpc_listen_port = 0;
+        };
+        clients = [
+          {
+            url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
+          }
+        ];
+        scrape_configs = [
+          {
+            job_name = "caddy";
+            static_configs = [
+              {
+                targets = [
+                  "localhost"
+                ];
+                labels = {
+                  job = "caddy";
+                  __path__ = "/var/log/caddy/*log";
+                  agent = "caddy-promtail";
+                };
+              }
+            ];
+            pipeline_stages = [
+              {
+                json = {
+                  expressions = {
+                    duration = "duration";
+                    status = "status";
+                  };
+                };
+              }
+              {
+                labels = {
+                  duration = null;
+                  status = null;
+                };
+              }
+            ];
+          }
+        ];
+      };
     };
   };
 }
