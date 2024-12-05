945eb6672260810b079572b434889cbbcebbad3b
Harsh Shandilya <me@msfjarvis.dev>
2024-12-06 01:35:28 +0530

    cyberdrop-dl: 5.7.2-unstable-2024-11-26 -> 5.7.2-unstable-2024-12-04

--- packages/cyberdrop-dl/default.nix
+++ packages/cyberdrop-dl/default.nix
@@ -6,21 +6,30 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "cyberdrop-dl";
-  version = "5.7.2-unstable-2024-11-26";
+  version = "5.7.2-unstable-2024-12-04";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jbsparrow";
-    repo = "cyberdropdownloader";
-    rev = "aab0f9cf59a3dba2857efcb39855f8de64c30e7e";
-    hash = "sha256-jd4VNthqoOlj8XWZdhkM71EEW4+jeVDAs7uZffaS1tU=";
+    repo = "CyberDropDownloader";
+    rev = "490631aacc0a6d98e0ef4f38d8b995f3173adf07";
+    hash = "sha256-O8lPRUeCqzvccKg9nipCYqOV23b1nPKE/CkoBYe0CjI=";
   };
 
   patches = [ ./unpin-dependencies.patch ];
 
-  nativeBuildInputs = [ python3.pkgs.poetry-core ];
+  postPatch = ''
+    # pyreadline3 is Windows-only
+    substituteInPlace pyproject.toml \
+      --replace-fail 'pyreadline3 = "^3.5.4"' ""
+  '';
 
-  propagatedBuildInputs = with python3.pkgs; [
+  build-system = [
+    python3.pkgs.poetry-core
+  ];
+
+  dependencies = with python3.pkgs; [
+    aenum
     aiofiles
     aiohttp
     aiolimiter
@@ -29,30 +38,35 @@ python3.pkgs.buildPythonApplication rec {
     arrow
     asyncpraw
     beautifulsoup4
-    browser-cookie3
     certifi
     filedate
     get-video-properties
-    humanfriendly
     inquirerpy
+    jeepney
+    lz4
     mediafire
     mutagen
     myjdapi
     pillow
     platformdirs
+    pycryptodomex
+    pydantic
     pyyaml
     rich
     send2trash
     xxhash
   ];
 
-  pythonImportsCheck = [ "cyberdrop_dl" ];
+  pythonImportsCheck = [
+    "cyberdrop_dl"
+  ];
 
-  meta = with lib; {
+  meta = {
     description = "Bulk Gallery Downloader for Cyberdrop.me and Other Sites";
-    homepage = "https://github.com/jbsparrow/cyberdropdownloader";
-    license = licenses.gpl3Only;
-    maintainers = with maintainers; [ ];
+    homepage = "https://github.com/jbsparrow/CyberDropDownloader";
+    changelog = "https://github.com/jbsparrow/CyberDropDownloader/blob/${src.rev}/CHANGELOG.md";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ ];
     mainProgram = "cyberdrop-dl";
   };
 }
