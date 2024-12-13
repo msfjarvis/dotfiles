1a357415c6f1f3ccf223c13b91d102e956b2752f
Harsh Shandilya <me@msfjarvis.dev>
2024-12-13 10:32:43 +0530

    darwin: pull in config goodies from uncenter

--- modules/darwin/homebrew/default.nix
+++ modules/darwin/homebrew/default.nix
@@ -1,10 +1,15 @@
 {
+  environment.variables = {
+    HOMEBREW_NO_ANALYTICS = "1";
+    HOMEBREW_NO_INSECURE_REDIRECT = "1";
+    HOMEBREW_NO_EMOJI = "1";
+  };
   homebrew = {
     enable = true;
     onActivation = {
       autoUpdate = true;
+      cleanup = "uninstall";
       upgrade = true;
-      cleanup = "zap";
     };
     brews = [
       "cocoapods"
--- systems/aarch64-darwin/Harshs-MacBook-Pro/default.nix
+++ systems/aarch64-darwin/Harshs-MacBook-Pro/default.nix
@@ -52,6 +52,18 @@
   # Allow sudo with Touch ID
   security.pam.enableSudoTouchIdAuth = true;
 
+  system.activationScripts = {
+    # https://github.com/ryan4yin/nix-darwin-kickstarter/blob/f79b4d4cbd9c8c2ea56b6e5016131ff2179d3775/minimal/modules/system.nix#L14-L19
+    postUserActivation.text = ''
+      killall Dock
+      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
+  };
+
+  security.sudo.extraConfig = ''
+    Defaults    env_keep += "TERMINFO"
+  '';
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
