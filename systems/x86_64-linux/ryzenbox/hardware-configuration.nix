f54646d949c979edad743710209ca903df65aecc
Harsh Shandilya <me@msfjarvis.dev>
2025-01-02 13:21:45 +0530

    ryzenbox: try the proprietary NVIDIA driver instead

--- systems/x86_64-linux/ryzenbox/hardware-configuration.nix
+++ systems/x86_64-linux/ryzenbox/hardware-configuration.nix
@@ -38,7 +38,7 @@ in
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
