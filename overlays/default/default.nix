3e23ba08976b144752ff3f4a9e7ea93e70fce922
Harsh Shandilya <me@msfjarvis.dev>
2025-01-09 18:03:48 +0530

    overlays: patch `nix-update` with my PR

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -1,5 +1,5 @@
 { inputs, ... }:
-_final: prev: {
+final: prev: {
   inherit (inputs.firefox.packages.${prev.stdenv.hostPlatform.system}) firefox-nightly-bin;
   # Force the use of the JDK we're using everywhere else
   jdk = prev.openjdk23;
@@ -10,6 +10,15 @@ _final: prev: {
   # Silence warnings about existing files
   megatools = prev.megatools.overrideAttrs (_: {
     patches = [ ./megatools.patch ];
+  });
+  nix-update = (prev.nix-update.override { nix = final.lix; }).overrideAttrs (_: {
+    patches = [
+      # https://github.com/Mic92/nix-update/pull/317
+      (prev.fetchpatch2 {
+        url = "https://github.com/Mic92/nix-update/commit/8e9c62b7649ef5b0c1d8522ba9e7ffc4aa248ba4.patch?full_index=1";
+        hash = "sha256-NtOGL5DexeCKYqUZid+EEEm3q28yVsE6qcJztgU0nsI=";
+      })
+    ];
   });
   qbittorrent = prev.qbittorrent.override { guiSupport = false; };
 }
