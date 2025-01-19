d1abdb1ca46fdf80bec933d8917cbf944bb6788d
Harsh Shandilya <me@msfjarvis.dev>
2025-01-19 18:21:08 +0530

    lix: upgrade to 2.92.0
    Flake lock file updates:
    
    • Updated input 'lix':
        'git+https://git.lix.systems/lix-project/lix?ref=refs/tags/2.91.1&rev=ad9d06f7838a25beec425ff406fe68721fef73be' (2024-10-19)
      → 'git+https://git.lix.systems/lix-project/lix?ref=refs/tags/2.92.0&rev=2837da71ec1588c1187d2e554719b15904a46c8b' (2025-01-18)

--- flake.nix
+++ flake.nix
@@ -121,7 +121,7 @@
     lib-aggregate.inputs.nixpkgs-lib.follows = "nixpkgs";
     lib-aggregate.inputs.flake-utils.follows = "flake-utils";
 
-    lix.url = "git+https://git.lix.systems/lix-project/lix?ref=refs/tags/2.91.1";
+    lix.url = "git+https://git.lix.systems/lix-project/lix?ref=refs/tags/2.92.0";
     lix.inputs.nixpkgs.follows = "nixpkgs";
     lix.inputs.nixpkgs-regression.follows = "nixpkgs";
     lix.inputs.nix2container.follows = "";
