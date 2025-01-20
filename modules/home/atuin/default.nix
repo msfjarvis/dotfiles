40d16de2a74bf1c95b58e24504b2c9fad2081e5d
Harsh Shandilya <me@msfjarvis.dev>
2025-01-20 21:45:01 +0530

    atuin: tweak layout again

--- modules/home/atuin/default.nix
+++ modules/home/atuin/default.nix
@@ -5,12 +5,12 @@
     flags = [ "--disable-up-arrow" ];
     settings = {
       auto_sync = true;
-      invert = true;
+      invert = false;
       max_preview_height = 2;
       search_mode = "fuzzy";
       show_preview = true;
       store_failed = false;
-      style = "compact";
+      style = "full";
       sync_frequency = "5m";
       sync_address = "http://wailord:8888";
     };
