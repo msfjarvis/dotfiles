a6c6c53cfec60d2ceea50af9c6284c11d972273a
Harsh Shandilya <me@msfjarvis.dev>
2025-02-07 19:36:01 +0530

    nixos/melody: init

---
+++ systems/aarch64-linux/melody/default.nix
@@ -0,0 +1,62 @@
+{
+  lib,
+  pkgs,
+  inputs,
+  namespace,
+  ...
+}:
+{
+  imports = [
+    ./disk-config.nix
+    inputs.nixos-facter-modules.nixosModules.facter
+    {
+      facter.reportPath = ./facter.json;
+      facter.detected.graphics.enable = false;
+    }
+  ];
+
+  boot = {
+    # Only enable for first installation
+    loader.efi.canTouchEfiVariables = true;
+    tmp.cleanOnBoot = true;
+  };
+  zramSwap.enable = true;
+
+  topology.self.name = "oracle server";
+
+  profiles.${namespace} = {
+    server = {
+      enable = true;
+      tailscaleExitNode = true;
+    };
+  };
+  networking.hostName = "melody";
+
+  time.timeZone = "Asia/Kolkata";
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = lib.mkForce "us";
+    useXkbConfig = true;
+  };
+
+  users = {
+    mutableUsers = false;
+    users = {
+      msfjarvis = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+        hashedPassword = ''$y$j9T$g8JL/B98ogQF/ryvwHpWe.$jyKMeotGz/o8Pje.nejKzPMiYOxtn//33OzMu5bAHm2'';
+      };
+    };
+  };
+
+  programs.command-not-found.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    attic-client
+    megatools
+  ];
+
+  system.stateVersion = "25.05";
+}
