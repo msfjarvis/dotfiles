e479713b3e06451b207254554fc3e9f0446bd6ad
Harsh Shandilya <me@msfjarvis.dev>
2025-02-09 00:12:09 +0530

    melody: use a round disk size

--- systems/aarch64-linux/melody/disk-config.nix
+++ systems/aarch64-linux/melody/disk-config.nix
@@ -9,7 +9,7 @@
           partitions = {
             ESP = {
               type = "EF00";
-              size = "500M";
+              size = "512M";
               content = {
                 type = "filesystem";
                 format = "vfat";
