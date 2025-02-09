d198bb8e0bf9e1f0dc299d3ee64b727813fff8a9
Harsh Shandilya <me@msfjarvis.dev>
2025-02-10 01:53:10 +0530

    shells/arduino-ide: init

---
+++ shells/arduino-ide/default.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+(pkgs.buildFHSEnv {
+  name = "arduino-env";
+  targetPkgs =
+    pkgs:
+    with pkgs;
+    [
+      ncurses
+      arduino-ide
+      zlib
+      (python3.withPackages (ps: [
+        ps.pyserial
+      ]))
+    ];
+  multiPkgs = null;
+}).env
