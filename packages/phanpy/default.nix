ce8fefd6bd64a8d72d9025eb5040372590f25921
Harsh Shandilya <me@msfjarvis.dev>
2025-03-03 11:09:26 +0530

    phanpy: 2025.01.26.24f03f5-unstable-2025-02-28 -> 2025.01.26.24f03f5-unstable-2025-03-03
    Diff: https://github.com/cheeaun/phanpy/compare/23e9d034e95f31b9dd7c71d74340b6555f804c18...333142aa67d863615560e4e601eee9ed2621ee51

--- packages/phanpy/default.nix
+++ packages/phanpy/default.nix
@@ -6,13 +6,13 @@
 }:
 buildNpmPackage {
   name = "phanpy";
-  version = "2025.01.26.24f03f5-unstable-2025-02-28";
+  version = "2025.01.26.24f03f5-unstable-2025-03-03";
 
   src = fetchFromGitHub {
     owner = "cheeaun";
     repo = "phanpy";
-    rev = "23e9d034e95f31b9dd7c71d74340b6555f804c18";
-    hash = "sha256-LEPiydF/lc5BsHhaCXHq6kC8CnkZcRYCig49J0vQLcQ=";
+    rev = "333142aa67d863615560e4e601eee9ed2621ee51";
+    hash = "sha256-5VrQElNkeoEUrqAkuaKhkBMRHUKLKiNYTpMrlChd90w=";
   };
 
   npmDepsHash = "sha256-1faItl+LsUGmBmznUSog3TLRYbbCCadFzlpa30w/xOQ=";
