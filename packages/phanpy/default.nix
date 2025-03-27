5eb7e9c5029df9399779e3ecafe33560f2b1c1b2
Harsh Shandilya <me@msfjarvis.dev>
2025-03-27 13:05:36 +0530

    phanpy: add a patch for boost+like action

--- packages/phanpy/default.nix
+++ packages/phanpy/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildNpmPackage,
   fetchFromGitHub,
+  fetchpatch2,
   defaultUrl ? "https://fedi.msfjarvis.dev",
 }:
 buildNpmPackage {
@@ -14,6 +15,14 @@ buildNpmPackage {
     rev = "d608ee0a7594e3c83cdb087e81002f176d0d7008";
     hash = "sha256-zaM1t4aeKvQnav/vO7iiNyrenxI1S3yvMH+mU4qqGtc=";
   };
+
+  patches = [
+    (fetchpatch2 {
+      # https://github.com/cheeaun/phanpy/issues/1085
+      url = "https://github.com/sm32d/phanpy/commit/1d50be6834d68d09a76b47e23b7584dd5b0cd4e6.patch";
+      hash = "sha256-lVH3ZcnnJen34dLxSMYp5D8QQCRPRSEZIJPrg2FiA28=";
+    })
+  ];
 
   npmDepsHash = "sha256-1faItl+LsUGmBmznUSog3TLRYbbCCadFzlpa30w/xOQ=";
 
