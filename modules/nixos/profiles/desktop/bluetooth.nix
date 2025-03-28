ec2e3cd752d0d4088bbb3d64bc8351c11e4c88e8
Harsh Shandilya <me@msfjarvis.dev>
2025-03-29 01:32:03 +0530

    bluetooth: tweak settings

--- modules/nixos/profiles/desktop/bluetooth.nix
+++ modules/nixos/profiles/desktop/bluetooth.nix
@@ -16,9 +16,11 @@ in
     services.blueman.enable = true;
     hardware.bluetooth = {
       enable = true;
+      powerOnBoot = true;
       settings = {
         General = {
           Enable = "Source,Sink,Media,Socket";
+          Experimental = true;
         };
       };
     };
