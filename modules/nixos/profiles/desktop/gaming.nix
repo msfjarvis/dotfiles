1a8e333cad08edb69ca51453fd675113c422e563
Harsh Shandilya <me@msfjarvis.dev>
2025-03-29 01:39:00 +0530

    gaming: enable better freetype rendering

--- modules/nixos/profiles/desktop/gaming.nix
+++ modules/nixos/profiles/desktop/gaming.nix
@@ -144,5 +144,10 @@ in
     ];
     # Required to avoid some logspew
     environment.sessionVariables.VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
+    environment.variables = {
+      # better fonts:
+      # https://web.archive.org/web/20230921201835/https://old.reddit.com/r/linux_gaming/comments/16lwgnj/is_it_possible_to_improve_font_rendering_on_linux/
+      FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
+    };
   };
 }
