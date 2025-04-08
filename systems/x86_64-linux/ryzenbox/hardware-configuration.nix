f93e2a2d5cb5c5518c31d5770a096a554abeb38f
Harsh Shandilya <me@msfjarvis.dev>
2025-04-08 14:59:19 +0530

    ryzenbox: revert to NVIDIA driver v565

--- systems/x86_64-linux/ryzenbox/hardware-configuration.nix
+++ systems/x86_64-linux/ryzenbox/hardware-configuration.nix
@@ -39,7 +39,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   disko.devices = {
