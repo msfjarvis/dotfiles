d350d84086d949a263575bbb0a7e5910710c567a
Harsh Shandilya <me@msfjarvis.dev>
2025-04-19 14:25:37 +0530

    matara: drop unnecessary MBR partition

--- systems/x86_64-linux/matara/hardware-configuration.nix
+++ systems/x86_64-linux/matara/hardware-configuration.nix
@@ -13,11 +13,6 @@
         content = {
           type = "gpt";
           partitions = {
-            MBR = {
-              type = "EF02"; # for grub MBR
-              size = "1M";
-              priority = 1; # Needs to be first partition
-            };
             ESP = {
               type = "EF00";
               size = "500M";
