143687d04d79bc426b140c09984f3809154e3b0c
Harsh Shandilya <me@msfjarvis.dev>
2025-04-20 19:20:11 +0530

    caddy-with-plugins: bump hash

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -4,5 +4,5 @@ caddy.withPlugins {
     "github.com/jasonlovesdoggo/caddy-defender@v0.8.5"
     "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556"
   ];
-  hash = "sha256-z+zj3rfXbyxldRjO1yoLD77ACRWEAofzMDiZe/bHAqw=";
+  hash = "sha256-KKNSP0lU4WgU1Viu1dcJEa5YH0Sgg9w5Au/2y9Jq6Ps=";
 }
