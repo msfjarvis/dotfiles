974eb9b8c31bb23ffa55dbfc76a18cb1ecdaa93e
Harsh Shandilya <me@msfjarvis.dev>
2025-04-21 19:11:57 +0530

    phanpy: update pinned hash for patch

--- packages/phanpy/default.nix
+++ packages/phanpy/default.nix
@@ -20,7 +20,7 @@ buildNpmPackage {
     (fetchpatch2 {
       # https://github.com/cheeaun/phanpy/issues/1085
       url = "https://github.com/sm32d/phanpy/commit/1d50be6834d68d09a76b47e23b7584dd5b0cd4e6.patch";
-      hash = "sha256-lVH3ZcnnJen34dLxSMYp5D8QQCRPRSEZIJPrg2FiA28=";
+      hash = "sha256-C3L10o0s7dx7bRMoqOOGSyA3oLfMyVMSRlvgd/AAG1c=";
     })
   ];
 
