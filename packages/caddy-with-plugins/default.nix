24584bb0aaa3c3735fee28d6e0df4a0502360ab1
Harsh Shandilya <me@msfjarvis.dev>
2025-04-25 01:53:10 +0530

    caddy: refresh hash

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -4,5 +4,5 @@ caddy.withPlugins {
     "github.com/jasonlovesdoggo/caddy-defender@v0.8.5"
     "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556"
   ];
-  hash = "sha256-KKNSP0lU4WgU1Viu1dcJEa5YH0Sgg9w5Au/2y9Jq6Ps=";
+  hash = "sha256-wy0VyV0v2POwC2OyV+cVHkl9JFpJcYG92fuCT/LDPZ8=";
 }
