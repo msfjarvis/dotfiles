b7108d4ac6ec0a8bf929b6e1e05e16586c353e9c
Harsh Shandilya <me@msfjarvis.dev>
2025-05-02 14:40:22 +0530

    server: use `extraSetFlags` for tailscale instead

--- modules/nixos/profiles/server/default.nix
+++ modules/nixos/profiles/server/default.nix
@@ -107,8 +107,10 @@ in
       extraUpFlags = [
         "--accept-risk=lose-ssh"
         "--ssh"
-        "--accept-dns=false" # I don't want my servers to use NextDNS
       ] ++ lib.optionals cfg.tailscaleExitNode [ "--advertise-exit-node" ];
+      extraSetFlags = [
+        "--accept-dns=false" # I don't want my servers to use NextDNS
+      ];
       useRoutingFeatures = if cfg.tailscaleExitNode then "both" else "client";
     };
   };
