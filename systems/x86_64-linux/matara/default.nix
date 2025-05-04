0b53b461a9aed7508397ee6f5405a4be7d556bd1
Harsh Shandilya <me@msfjarvis.dev>
2025-05-04 18:02:52 +0530

    matara: remove duplicate prometheus config

--- systems/x86_64-linux/matara/default.nix
+++ systems/x86_64-linux/matara/default.nix
@@ -79,18 +79,6 @@
     };
   };
 
-  services.prometheus = {
-    enable = true;
-    port = 9001;
-    exporters = {
-      node = {
-        enable = true;
-        enabledCollectors = [ "systemd" ];
-        port = 9002;
-      };
-    };
-  };
-
   services.${namespace} = {
     gphotos-cdp =
       let
