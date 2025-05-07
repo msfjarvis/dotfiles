31067cd9236bb1431472721c6db2e8845994d1a7
Harsh Shandilya <me@msfjarvis.dev>
2025-05-07 21:05:56 +0530

    nix: set `netrc-file`

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   namespace,
@@ -17,12 +18,19 @@
     flake = "/home/msfjarvis/git-repos/dotfiles";
   };
 
+  sops.secrets.nix-netrc-file = {
+    sopsFile = lib.snowfall.fs.get-file "secrets/nix-cache.yaml";
+    format = "yaml";
+    key = "netrc-file";
+  };
+
   nix =
     lib.${namespace}.mkNixConfig {
       inherit lib pkgs;
     }
     // {
       optimise.automatic = false;
+      settings.netrc-file = config.sops.secrets.nix-netrc-file.path;
     };
 
   system.switch = {
