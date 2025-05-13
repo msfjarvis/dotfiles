e54d405ce2337c251edfb220faea3033bfd2cf8d
Harsh Shandilya <me@msfjarvis.dev>
2025-05-13 23:14:02 +0530

    homes: enable support for standalone building
    ugly but works

--- homes/aarch64-linux/msfjarvis/default.nix
+++ homes/aarch64-linux/msfjarvis/default.nix
@@ -6,6 +6,11 @@
   };
 
   profiles.${namespace}.starship.server = true;
+  nix.extraOptions = ''
+    substituters = https://nix-cache.tiger-shark.ts.net/aarch64-linux https://cache.nixos.org
+    trusted-public-keys = aarch64-linux:czBXxHtNIDorynmG/2pRuFSENM+fnu0rgVkH+8I4niQ= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
+    netrc-file = /home/msfjarvis/.config/nix/netrc
+  '';
 
   home.stateVersion = "21.05";
 }
