f8424085960ed9a3d1c5074b8d5e112c092dce38
Harsh Shandilya <me@msfjarvis.dev>
2025-05-18 13:35:09 +0530

    caddy: update dep hash

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -4,5 +4,5 @@ caddy.withPlugins {
     "github.com/jasonlovesdoggo/caddy-defender@v0.8.5"
     "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556"
   ];
-  hash = "sha256-wy0VyV0v2POwC2OyV+cVHkl9JFpJcYG92fuCT/LDPZ8=";
+  hash = "sha256-e/V9X19Nkh1SjNC53C7gCxsWkZA3ZrhwP7Ujlj+3Q3s=";
 }
