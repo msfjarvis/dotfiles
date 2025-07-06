ae925337cf4d642a5d07b85ef4669cb0647f7b65
Harsh Shandilya <me@msfjarvis.dev>
2025-07-06 11:42:49 +0530

    caddy: add plausible plugin

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -3,6 +3,7 @@ caddy.withPlugins {
   plugins = [
     "pkg.jsn.cam/caddy-defender@v0.0.0-20250706022202-8b80591478db"
     "github.com/tailscale/caddy-tailscale@v0.0.0-20250508175905-642f61fea3cc"
+    "github.com/muety/caddy-plausible-plugin@v0.0.0-20250530190508-579fbc2c77a7"
   ];
-  hash = "sha256-aARMndSsHXFXh2AYa7rFRuP71HjQXigZJk2mb3d9O6g=";
+  hash = "sha256-E8sf1X+y52ZSY2cwg3jBiBfTBgg/4hhFhWYuhmdxREU=";
 }
