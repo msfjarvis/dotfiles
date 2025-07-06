bca9cfcee5d350db2e52b5d9abb224156aa0f577
Harsh Shandilya <me@msfjarvis.dev>
2025-07-06 21:45:33 +0530

    gitea: remove duplicated logging config

--- modules/nixos/gitea/default.nix
+++ modules/nixos/gitea/default.nix
@@ -28,15 +28,6 @@ in
         extraConfig = ''
           import blackholeCrawlers
           reverse_proxy 127.0.0.1:${toString config.services.gitea.settings.server.HTTP_PORT}
-          log {
-            output file /var/log/caddy/caddy_${cfg.domain}.log {
-              roll_size 100MiB
-              roll_keep 5
-              roll_keep_for 100d
-            }
-            format json
-            level INFO
-          }
         '';
       };
     };
