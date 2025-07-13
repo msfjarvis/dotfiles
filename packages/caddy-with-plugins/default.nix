921ec1a8ccdf610d84f45b8fc68a293e61127fed
Harsh Shandilya <me@msfjarvis.dev>
2025-07-13 19:51:49 +0530

    caddy: update caddy-defender

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -1,9 +1,9 @@
 { caddy, ... }:
 caddy.withPlugins {
   plugins = [
-    "pkg.jsn.cam/caddy-defender@v0.0.0-20250706022202-8b80591478db"
+    "pkg.jsn.cam/caddy-defender@v0.0.0-20250713022654-754d5e0a690d"
     "github.com/tailscale/caddy-tailscale@v0.0.0-20250508175905-642f61fea3cc"
     "github.com/muety/caddy-plausible-plugin@v0.0.0-20250530190508-579fbc2c77a7"
   ];
-  hash = "sha256-E8sf1X+y52ZSY2cwg3jBiBfTBgg/4hhFhWYuhmdxREU=";
+  hash = "sha256-fcY8973CKg3c+C9vrtb1Bqs0sWGrfxT0VHx5MlHoLGs=";
 }
