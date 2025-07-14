e184ddfeb3fe8ed14ec462e61a4e94074a8d6f6f
Harsh Shandilya <me@msfjarvis.dev>
2025-07-14 15:34:20 +0530

    caddy: add gitea pages plugin

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -4,6 +4,7 @@ caddy.withPlugins {
     "pkg.jsn.cam/caddy-defender@v0.0.0-20250713022654-754d5e0a690d"
     "github.com/tailscale/caddy-tailscale@v0.0.0-20250508175905-642f61fea3cc"
     "github.com/muety/caddy-plausible-plugin@v0.0.0-20250530190508-579fbc2c77a7"
+    "github.com/rsp2k/caddy-gitea-pages@v0.0.0-20250609073252-9bb99965619a"
   ];
-  hash = "sha256-fcY8973CKg3c+C9vrtb1Bqs0sWGrfxT0VHx5MlHoLGs=";
+  hash = "sha256-mJ2dqORg3c3AkMm8B5W+ihbkR2PHU88p6pM/6ZRnrTI=";
 }
