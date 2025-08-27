fd95436238d099731f553f469fd1ff58fb4ec0e4
Harsh Shandilya <me@msfjarvis.dev>
2025-08-27 18:44:27 +0530

    caddy-with-plugins: add caddy-security

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -5,6 +5,7 @@ caddy.withPlugins {
     "github.com/muety/caddy-plausible-plugin@v0.0.0-20250530190508-579fbc2c77a7"
     "github.com/rsp2k/caddy-gitea-pages@v0.0.0-20250609073252-9bb99965619a"
     "github.com/msfjarvis/caddy-tailscale@v0.0.0-20250723144432-4e8672b140b0"
+    "github.com/greenpau/caddy-security@v0.0.0-20250325161856-83609dec14a4"
   ];
-  hash = "sha256-vycci2tngwbx2TLdO5diw93Wm8WMUx/5Vx6b0YWeJfw=";
+  hash = "sha256-Dgb8UjPP6ZKJ5jHj0lTO2MNdFpjmqjdub4S/7YryKkk=";
 }
