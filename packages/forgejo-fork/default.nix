0fb72e3a97f94d5288faed433b5d3ada0056a0e5
Harsh Shandilya <me@msfjarvis.dev>
2025-10-02 12:35:37 +0530

    forgejo-fork: re-roll patch list

--- packages/forgejo-fork/default.nix
+++ packages/forgejo-fork/default.nix
@@ -15,13 +15,8 @@ in
 forgejo.overrideAttrs {
   patches = [
     # Add `/user/repo/commit` route
-    (mkPatch "1176b414fe76a04c9eaa19fe355826fee87ad13e" "sha256-Xd/CFVbuiedNILyjgeT3bImm7FyDd1beA//4YApS1eQ=")
-    # Render external commit links with proper prefixes
-    # Also fixes a host of Markdown rendering bugs
-    # https://codeberg.org/forgejo/forgejo/pulls/9146
-    (fetchpatch2 {
-      url = "https://codeberg.org/forgejo/forgejo/pulls/9146.patch";
-      hash = "sha256-u6dvptIy0DHFsN7ARE0opMt0vYYhH+XT0YAZb6rBS1Y=";
-    })
+    (mkPatch "6a8a1a7189e33b8799b422f5601f791ae75799ea" "sha256-Xd/CFVbuiedNILyjgeT3bImm7FyDd1beA//4YApS1eQ=")
+    # Improved rendering of commit links
+    (mkPatch "b40ed4b106e6501a880fe284a1fc51599920450d" "sha256-IKKxigZYZBLkfs5Zn5nGX7T57CE1i6bsBmsXEtXvecQ=")
   ];
 }
