7aef4f627cd78956634605c2138487e2df1f630f
GitHub Action <action@github.com>
2025-10-19 01:37:02 +0000

    chore: update Caddy plugins

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -1,11 +1,11 @@
 { caddy, ... }:
 caddy.withPlugins {
   plugins = [
-    "pkg.jsn.cam/caddy-defender@v0.0.0-20251011013218-9babf69890e2"
-    "github.com/muety/caddy-plausible-plugin@v0.0.0-20250829143804-5041083e02a1"
+    "pkg.jsn.cam/caddy-defender@v0.0.0-20251012015712-72a06b33560d"
+    "github.com/muety/caddy-plausible-plugin@v0.0.0-20251017124604-ca540216a266"
     "github.com/rsp2k/caddy-gitea-pages@v0.0.0-20250609073252-9bb99965619a"
     "github.com/msfjarvis/caddy-tailscale@v0.0.0-20250723144432-4e8672b140b0"
     "github.com/greenpau/caddy-security@v0.0.0-20250325161856-83609dec14a4"
   ];
-  hash = "sha256-yhuT98J8HDYSNR5cY/lGv0iy08MJUKJ/ANtSNma9BN0=";
+  hash = "sha256-v0HNRBST80fja/5dfoes3u+TSR8KkpwcFMbyDa7N1Ww=";
 }
