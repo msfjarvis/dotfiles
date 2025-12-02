2d0d684d4f1c7778d802c6b3115c9f75e972f377
Harsh Shandilya <me@msfjarvis.dev>
2025-12-03 02:04:48 +0530

    forgejo: disable downloading source code archives

--- modules/nixos/forgejo/default.nix
+++ modules/nixos/forgejo/default.nix
@@ -121,7 +121,7 @@ in
           DISABLE_HTTP_GIT = false;
           DISABLE_STARS = true;
           ENABLE_PUSH_CREATE_USER = true;
-          DISABLE_DOWNLOAD_SOURCE_ARCHIVES = false;
+          DISABLE_DOWNLOAD_SOURCE_ARCHIVES = true;
         };
         "repository.pull-request" = {
           DEFAULT_MERGE_STYLE = "rebase";
