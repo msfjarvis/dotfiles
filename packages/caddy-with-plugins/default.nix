84a36bff563b453181a5b11b87e8c953984395a2
Harsh Shandilya <me@msfjarvis.dev>
2025-12-14 12:21:46 +0530

    caddy: update plugins

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -1,11 +1,11 @@
 { caddy, ... }:
 caddy.withPlugins {
   plugins = [
-    "pkg.jsn.cam/caddy-defender@v0.0.0-20251203214511-038f7afde8b6"
+    "pkg.jsn.cam/caddy-defender@v0.0.0-20251214022152-caac4b6be0d4"
     "github.com/muety/caddy-plausible-plugin@v0.0.0-20251017124604-ca540216a266"
     "github.com/rsp2k/caddy-gitea-pages@v0.0.0-20250609073252-9bb99965619a"
     "github.com/msfjarvis/caddy-tailscale@v0.0.0-20251120182636-39d279ab4233"
     "github.com/greenpau/caddy-security@v0.0.0-20250325161856-83609dec14a4"
   ];
-  hash = "sha256-JBBRzZQc5yyz1RRopTMnh52LmRav/SKD0RZ8pGmsOFM=";
+  hash = "sha256-lTnvY73PMGT/JlpraTGxcxfhq79rZMdrC/aFz78/zAQ=";
 }
