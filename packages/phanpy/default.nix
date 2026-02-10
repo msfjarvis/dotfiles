f6328d52e85824ff73e339c25cad9d6a87f61410
Harsh Shandilya <me@msfjarvis.dev>
2026-02-10 09:10:07 +0530

    phanpy: temporarily disable patch
    Will fix this later

--- packages/phanpy/default.nix
+++ packages/phanpy/default.nix
@@ -18,11 +18,11 @@ buildNpmPackage {
   };
 
   patches = [
-    (fetchpatch2 {
-      # https://github.com/cheeaun/phanpy/issues/1085
-      url = "https://github.com/sm32d/phanpy/commit/1d50be6834d68d09a76b47e23b7584dd5b0cd4e6.patch";
-      hash = "sha256-C3L10o0s7dx7bRMoqOOGSyA3oLfMyVMSRlvgd/AAG1c=";
-    })
+    # (fetchpatch2 {
+    #   # https://github.com/cheeaun/phanpy/issues/1085
+    #   url = "https://github.com/sm32d/phanpy/commit/1d50be6834d68d09a76b47e23b7584dd5b0cd4e6.patch";
+    #   hash = "sha256-C3L10o0s7dx7bRMoqOOGSyA3oLfMyVMSRlvgd/AAG1c=";
+    # })
   ];
 
   npmDepsHash = "sha256-s0Z3/pJvdk5XOqVvqlr7vbGISjM9CwCp9ZW7JnQzH2U=";
