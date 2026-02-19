845f3def63643f32b4e61d884ac88d2da1a36254
Harsh Shandilya <me@msfjarvis.dev>
2026-02-19 21:39:37 +0530

    .

--- systems/aarch64-linux/wailord/glance.nix
+++ systems/aarch64-linux/wailord/glance.nix
@@ -84,10 +84,11 @@
               type = "custom-api";
               title = "Open Tasks";
               cache = "5m";
-              url = "https://caldav-api.tiger-shark.ts.net/todo";
+              url = "https://caldav-api.tiger-shark.ts.net/tasks";
               template = ''
                 <ul class="list list-gap-14 list-with-separator">
                 {{ range .JSON.Array "" }}
+                  {{ if eq (.String "status") "NEEDS-ACTION" }}
                   <li class="size-h3">
                     <a href="{{ .String "url" }}" target="_blank" class="color-primary-if-not-visited">
                       {{ .String "summary" }}
@@ -95,14 +96,17 @@
                     <div class="text-color-dimmed size-h5 margin-top-4">
                       {{ if .String "due" }}
                         Due <span {{ .String "due" | parseTime "RFC3339" | toRelativeTime }}></span>
+                      {{ else if .String "start" }}
+                        Starts <span {{ .String "start" | parseTime "RFC3339" | toRelativeTime }}></span>
                       {{ else }}
-                        No due date
+                        No date set
                       {{ end }}
                       {{ if .Exists "categories" }}
                         {{ range .Array "categories" }} • {{ .String "" }}{{ end }}
                       {{ end }}
                     </div>
                   </li>
+                  {{ end }}
                 {{ end }}
                 </ul>
               '';
