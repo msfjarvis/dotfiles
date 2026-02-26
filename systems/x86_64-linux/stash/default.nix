0a788d87e80e5a216d9cae9b4393d2a002b6c1f8
Harsh Shandilya <me@msfjarvis.dev>
2026-02-26 08:13:39 +0530

    provision stash microvm

---
+++ systems/x86_64-linux/stash/default.nix
@@ -0,0 +1,74 @@
+{
+  lib,
+  pkgs,
+  inputs,
+  namespace,
+  ...
+}:
+{
+  imports = [ inputs.microvm.nixosModules.microvm ];
+  networking.hostName = "stash";
+
+  microvm.interfaces = [
+    {
+      type = "tap";
+      id = "vm-a1";
+      mac = "02:00:00:00:00:01";
+    }
+  ];
+
+  microvm.shares = [
+    {
+      source = "/nix/store";
+      mountPoint = "/nix/.ro-store";
+      tag = "ro-store";
+      proto = "virtiofs";
+    }
+    {
+      source = "/mediahell/MEGA/Videos";
+      mountPoint = "/stash";
+      tag = "stash";
+      proto = "virtiofs";
+    }
+    {
+      source = "home";
+      mountPoint = "/home";
+      tag = "home";
+      proto = "virtiofs";
+    }
+  ];
+
+  users.users.msfjarvis.isSystemUser = true;
+  users.users.msfjarvis.group = "msfjarvis";
+  users.groups.msfjarvis = { };
+
+  services.stash = {
+    enable = true;
+    pythonPackage = pkgs.python3.withPackages (p: [
+      p.beautifulsoup4
+      p.cloudscraper
+      p.configparser
+      # p.libpath
+      p.lxml
+      p.progressbar
+      p.requests
+      p.yt-dlp
+      p.stashapi
+    ]);
+    jwtSecretKeyFile = "/home/stash-jwt";
+    sessionStoreKeyFile = "/home/stash-sess";
+    passwordFile = "/home/stash-pass";
+    username = "msfjarvis";
+    mutableSettings = true;
+    settings = {
+      ui.frontPageContent = [ ];
+      host = "127.0.0.1";
+      port = lib.${namespace}.ports.stash + 1000;
+      stash = [
+        {
+          path = "/stash";
+        }
+      ];
+    };
+  };
+}
