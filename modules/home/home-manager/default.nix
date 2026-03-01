56741a023f034c719f3291d8a3b857d9cae2daa1
Harsh Shandilya <me@msfjarvis.dev>
2026-03-01 18:19:51 +0530

    home-manager: add workaround for home-manager bug

--- modules/home/home-manager/default.nix
+++ modules/home/home-manager/default.nix
@@ -1,5 +1,18 @@
+{ config, lib, ... }:
 {
   programs.home-manager = {
     enable = true;
   };
+
+  # Workaround for https://github.com/nix-community/home-manager/issues/8786
+  home.activation.installPackages = lib.mkForce (
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      nixProfileRemove home-manager-path
+      if [[ -e ${config.home.profileDirectory}/manifest.json ]]; then
+        run nix profile install ${config.home.path}
+      else
+        run nix-env -i ${config.home.path}
+      fi
+    ''
+  );
 }
