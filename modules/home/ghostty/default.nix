10c491c84d35ce4366529ff74134ab98e9bc0563
Harsh Shandilya <me@msfjarvis.dev>
2026-03-11 01:57:26 +0530

    ghostty: check for stylix availability

--- modules/home/ghostty/default.nix
+++ modules/home/ghostty/default.nix
@@ -1,54 +1,62 @@
 {
   config,
   lib,
+  options,
   pkgs,
   namespace,
   ...
 }:
 let
   cfg = config.profiles.${namespace}.ghostty;
+  stylixAvailable = options ? stylix.targets;
   inherit (lib)
     mkEnableOption
     mkIf
+    mkMerge
+    optionalAttrs
     ;
 in
 {
   options.profiles.${namespace}.ghostty = {
     enable = mkEnableOption "ghostty, a fast, feature-rich, and cross-platform terminal emulator that uses platform-native UI and GPU acceleration.";
   };
-  config = mkIf cfg.enable {
-    stylix.targets.ghostty.enable = config.programs.ghostty.enable;
-    programs.ghostty = {
-      enable = true;
-      enableBashIntegration = true;
-      installBatSyntax = true;
-      # TODO: https://gist.github.com/jamesgecko/dd921cef7db3b9533cee4473e832f2a4
-      settings = {
-        cursor-click-to-move = true;
-        gtk-quick-terminal-layer = "overlay";
-        keybind = [
-          "ctrl+shift+right=unbind"
-          "ctrl+shift+left=unbind"
-          "shift+end=unbind"
-          "shift+home=unbind"
-        ];
-        maximize = true;
-        notify-on-command-finish = "unfocused";
-        notify-on-command-finish-action = "notify";
-        palette-generate = true;
-        # 512 MB in bytes
-        scrollback-limit = 536870912;
-        shell-integration = "bash";
-        shell-integration-features = true;
-        quit-after-last-window-closed = false;
-        window-theme = "ghostty";
+  config = mkIf cfg.enable (mkMerge [
+    {
+      programs.ghostty = {
+        enable = true;
+        enableBashIntegration = true;
+        installBatSyntax = true;
+        # TODO: https://gist.github.com/jamesgecko/dd921cef7db3b9533cee4473e832f2a4
+        settings = {
+          cursor-click-to-move = true;
+          gtk-quick-terminal-layer = "overlay";
+          keybind = [
+            "ctrl+shift+right=unbind"
+            "ctrl+shift+left=unbind"
+            "shift+end=unbind"
+            "shift+home=unbind"
+          ];
+          maximize = true;
+          notify-on-command-finish = "unfocused";
+          notify-on-command-finish-action = "notify";
+          palette-generate = true;
+          # 512 MB in bytes
+          scrollback-limit = 536870912;
+          shell-integration = "bash";
+          shell-integration-features = true;
+          quit-after-last-window-closed = false;
+          window-theme = "ghostty";
+        };
       };
-    };
-    dconf.settings = {
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = lib.mkForce {
-        binding = "<Control><Alt>t";
-        command = "${lib.getExe pkgs.ghostty}";
+      dconf.settings = {
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = lib.mkForce {
+          binding = "<Control><Alt>t";
+          command = "${lib.getExe pkgs.ghostty}";
+        };
       };
-    };
-  };
+    }
+    (optionalAttrs stylixAvailable {
+      stylix.targets.ghostty.enable = config.programs.ghostty.enable;
+    })
+  ]);
 }
