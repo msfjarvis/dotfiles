db123a7aa7b67165ed3a8bd1d632fb4dbc736f0f
Harsh Shandilya <me@msfjarvis.dev>
2026-03-16 03:25:56 +0530

    golink: collect Prometheus metrics

--- modules/nixos/golink/default.nix
+++ modules/nixos/golink/default.nix
@@ -7,6 +7,7 @@
 let
   cfg = config.services.${namespace}.golink;
   inherit (lib) mkEnableOption mkIf;
+  inherit (lib.${namespace}) tailnetDomain;
 in
 {
   options.services.${namespace}.golink = {
@@ -23,5 +24,17 @@ in
       enable = true;
       tailscaleAuthKeyFile = config.sops.secrets.golink-tsauthkey.path;
     };
+    services.prometheus.scrapeConfigs = [
+      {
+        job_name = "golink";
+        metrics_path = "/.metrics";
+        scheme = "https";
+        static_configs = [
+          {
+            targets = [ "go.${tailnetDomain}" ];
+          }
+        ];
+      }
+    ];
   };
 }
