f92bf97dd615a999c82dcaf039df11019b21feaa
GitHub Action <action@github.com>
2026-04-05 01:38:18 +0000

    chore: update Caddy plugins

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -1,11 +1,11 @@
 { caddy, ... }:
 caddy.withPlugins {
   plugins = [
-    "pkg.jsn.cam/caddy-defender@v0.0.0-20260329005309-b0764deddfb4"
+    "pkg.jsn.cam/caddy-defender@v0.0.0-20260405005332-1a65a821f900"
     "github.com/rsp2k/caddy-gitea-pages@v0.0.0-20250609073252-9bb99965619a"
-    "github.com/msfjarvis/caddy-tailscale@v0.0.0-20260329010337-d03a62f90b70"
-    "github.com/greenpau/caddy-security@v0.0.0-20260327001232-f13b8e6af116"
+    "github.com/msfjarvis/caddy-tailscale@v0.0.0-20260405010411-6d40db131e67"
+    "github.com/greenpau/caddy-security@v0.0.0-20260403154904-fb799584502f"
     "github.com/porech/caddy-maxmind-geolocation@v0.0.0-20250305164927-9066f91c9696"
   ];
-  hash = "sha256-2iT9H7yTbQtx1K3g/GbPjoEoNcW8b5jYA0ONt5r5fms=";
+  hash = "sha256-WiB3C5isQnNtSY7DcoEZ6ZT5dUEN/lFK1IA3Jc9siGk=";
 }
