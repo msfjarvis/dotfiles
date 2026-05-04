3e0303ea403daf5eeb0d9aaf0fb5025ef7a9ddae
Harsh Shandilya <me@msfjarvis.dev>
2026-05-04 23:44:52 +0530

    bump caddy-tailscale

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -3,9 +3,9 @@ caddy.withPlugins {
   plugins = [
     "pkg.jsn.cam/caddy-defender@v0.0.0-20260503010518-4ee6576e40af"
     "github.com/rsp2k/caddy-gitea-pages@v0.0.0-20250609073252-9bb99965619a"
-    "github.com/msfjarvis/caddy-tailscale@v0.0.0-20260503011730-493ff9288d56"
+    "github.com/msfjarvis/caddy-tailscale@v0.0.0-20260504180027-b397bea2e546"
     "github.com/greenpau/caddy-security@v0.0.0-20260425125447-e03784fa0581"
     "github.com/porech/caddy-maxmind-geolocation@v0.0.0-20250305164927-9066f91c9696"
   ];
-  hash = "sha256-EuKX6Y4NUW7UHdX1sxgl3spjGVTnMTGuCCTqhyZYBts=";
+  hash = "sha256-Uavi7h+16oxklnOO5Pjln/W5kqSp6tTAVcInlNWnWCI=";
 }
