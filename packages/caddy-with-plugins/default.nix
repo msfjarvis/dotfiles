0aa4258e7572c4b7e6c555b82eba88b0c6d6d623
Harsh Shandilya <me@msfjarvis.dev>
2026-05-22 21:13:37 +0530

    caddy: bump

--- packages/caddy-with-plugins/default.nix
+++ packages/caddy-with-plugins/default.nix
@@ -1,11 +1,11 @@
 { caddy, ... }:
 caddy.withPlugins {
   plugins = [
-    "pkg.jsn.cam/caddy-defender@v0.0.0-20260517010834-a8ec7e69208c"
+    "pkg.jsn.cam/caddy-defender@v0.0.0-20260519024046-c18914c5b60e"
     "github.com/rsp2k/caddy-gitea-pages@v0.0.0-20250609073252-9bb99965619a"
-    "github.com/msfjarvis/caddy-tailscale@v0.0.0-20260503011730-493ff9288d56"
+    "github.com/msfjarvis/caddy-tailscale@v0.0.0-20260522153743-9cc53305dea6"
     "github.com/greenpau/caddy-security@v0.0.0-20260425125447-e03784fa0581"
     "github.com/porech/caddy-maxmind-geolocation@v0.0.0-20250305164927-9066f91c9696"
   ];
-  hash = "sha256-boVZ/vLmbXWKzYcoml5B1ysjFQLzbUEKXT9W0yZQ1KE=";
+  hash = "sha256-J1TOE5Sp2t1x4HtkaUC/M3BmsDmj/jt8BEV3qilYVxk=";
 }
