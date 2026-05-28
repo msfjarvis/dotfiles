7447b528ed28300847093cb785975fa2fdfb214d
Harsh Shandilya <me@msfjarvis.dev>
2026-05-28 08:23:07 +0530

    overlays: drop upstreamed patch

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -5,13 +5,4 @@ _: prev: {
   megatools = prev.megatools.overrideAttrs (_: {
     patches = [ ./megatools.patch ];
   });
-  qbittorrent = (prev.qbittorrent.override { guiSupport = false; }).overrideAttrs (_: {
-    patches = [
-      (prev.fetchpatch2 {
-        # https://github.com/qbittorrent/qBittorrent/pull/24286
-        url = "https://patch-diff.githubusercontent.com/raw/qbittorrent/qBittorrent/pull/24286.patch";
-        hash = "sha256-Lb/ly92RApsunqoAQaqHezWCmEyvxn6tTKJcX4IQrWw=";
-      })
-    ];
-  });
 }
