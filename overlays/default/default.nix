2a9be6acfb66cfeaadf2bab742e1cda7960f34ec
Harsh Shandilya <me@msfjarvis.dev>
2026-06-10 08:59:30 +0530

    fmt

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -6,14 +6,14 @@ _: prev: {
     patches = [ ./megatools.patch ];
   });
   # rsync 3.4.3 is going through an infinite regression simulator thanks to Claude
-  rsync = prev.rsync.overrideAttrs(_: rec {
-  	version = "3.4.1";
+  rsync = prev.rsync.overrideAttrs (_: rec {
+    version = "3.4.1";
 
-  	src = prev.fetchurl {
-  	  # signed with key 9FEF 112D CE19 A0DC 7E88  2CB8 1BB2 4997 A853 5F6F
-  	  url = "mirror://samba/rsync/src/rsync-${version}.tar.gz";
-  	  hash = "sha256-KSS8s6Hti1UfwQH3QLnw/gogKxFQJ2R89phQ1l/YjFI=";
-  	};
+    src = prev.fetchurl {
+      # signed with key 9FEF 112D CE19 A0DC 7E88  2CB8 1BB2 4997 A853 5F6F
+      url = "mirror://samba/rsync/src/rsync-${version}.tar.gz";
+      hash = "sha256-KSS8s6Hti1UfwQH3QLnw/gogKxFQJ2R89phQ1l/YjFI=";
+    };
   });
   qbittorrent = prev.qbittorrent.override { guiSupport = false; };
 }
