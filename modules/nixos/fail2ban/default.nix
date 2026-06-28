77f353a40fa206efca556dd0200b8189d2dddf42
Harsh Shandilya <me@msfjarvis.dev>
2026-06-28 14:55:43 +0530

    fixup! disable cloudflare edge bans in fail2ban config

--- modules/nixos/fail2ban/default.nix
+++ modules/nixos/fail2ban/default.nix
@@ -13,8 +13,9 @@ let
     getExe
     mkEnableOption
     mkIf
+    mkMerge
     optional
-    optionals
+    optionalAttrs
     ;
   inherit (lib.${namespace}) ports;
   prometheusEnabled = config.services.${namespace}.prometheus.enable;
@@ -39,95 +40,100 @@ in
     cloudflare.enable = mkEnableOption "Cloudflare edge bans for public web jails";
   };
 
-  config = mkIf cfg.enable {
-    services.fail2ban.enable = true;
+  config = mkMerge [
+    {
+      services.fail2ban.enable = true;
 
-    sops.secrets.cloudflare-fail2ban-token = mkIf cfg.cloudflare.enable {
-      sopsFile = lib.snowfall.fs.get-file "secrets/cloudflare/fail2ban.yaml";
-      format = "yaml";
-      key = "token";
-    };
-    sops.secrets.cloudflare-fail2ban-zone-id = mkIf cfg.cloudflare.enable {
-      sopsFile = lib.snowfall.fs.get-file "secrets/cloudflare/fail2ban.yaml";
-      format = "yaml";
-      key = "zone_id";
-    };
+      environment.etc."fail2ban/filter.d/caddy-access.local".text = ''
+        [Definition]
+        failregex = ^<HOST>.*"(GET|POST|OPTIONS).*" (4[0-9][0-9])[ \d]*$
+        ignoreregex =
+      '';
 
-    environment.etc."fail2ban/filter.d/caddy-access.local".text = ''
-      [Definition]
-      failregex = ^<HOST>.*"(GET|POST|OPTIONS).*" (4[0-9][0-9])[ \d]*$
-      ignoreregex =
-    '';
-    environment.etc."fail2ban/action.d/cloudflare-edge-ban.conf".text = mkIf cfg.cloudflare.enable ''
-      [Definition]
-      actionstart =
-      actionstop =
-      actioncheck =
-      actionban = ${getExe pkgs.curl} -s -X POST "<_cf_api_url>" <_cf_api_prms> --data '{"mode":"<cfmode>","configuration":{"target":"<cftarget>","value":"<ip>"},"notes":"<notes>"}'
-      actionunban = id=$(${getExe pkgs.curl} -s -G -X GET "<_cf_api_url>" --data-urlencode "mode=<cfmode>" --data-urlencode "notes=<notes>" --data-urlencode "configuration.target=<cftarget>" --data-urlencode "configuration.value=<ip>" <_cf_api_prms> | ${getExe pkgs.gawk} -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/"id"/){print $(i+1)}}}' | tr -d ' "' | head -n 1)
-      	if [ -z "$id" ]; then echo "<name>: id for <ip> cannot be found using target <cftarget>"; exit 0; fi; \
-      	${getExe pkgs.curl} -s -X DELETE "<_cf_api_url>/$id" <_cf_api_prms> --data '{"cascade": "none"}'
-      _cf_api_url = https://api.cloudflare.com/client/v4/zones/$(cat <zoneidfile>)/firewall/access_rules/rules
-      _cf_api_prms = -H "Authorization: Bearer $(cat <tokenfile>)" -H "Content-Type: application/json"
-
-      [Init]
-      cftarget = ip
-      cfmode = block
-      notes = Fail2Ban <name>
-      tokenfile = ${config.sops.secrets.cloudflare-fail2ban-token.path}
-      zoneidfile = ${config.sops.secrets.cloudflare-fail2ban-zone-id.path}
-
-      [Init?family=inet6]
-      cftarget = ip6
-    '';
-
-    services.fail2ban.jails.caddy-access.settings = {
-      enabled = true;
-      filter = "caddy-access";
-      logpath = "/var/log/caddy/access-*.log";
-      backend = "auto";
-      port = "http,https";
-      findtime = 30;
-      maxretry = 5;
-      bantime = 600;
-    }
-    // lib.optionalAttrs cfg.cloudflare.enable {
-      action = lib.concatStringsSep "\n" [
-        "%(action_)s"
-        "  cloudflare-edge-ban"
-      ];
-    };
-
-    services.prometheus.exporters.fail2ban = {
-      enable = true;
-      host = "127.0.0.1";
-      port = ports.exporters.fail2ban;
-    };
-
-    systemd.services.prometheus-fail2ban-exporter = mkIf exporterCfg.enable {
-      requires = mkIf config.services.fail2ban.enable [ "prometheus-fail2ban-exporter-setup.service" ];
-      serviceConfig = {
-        DynamicUser = false;
-        ExecStart = lib.mkForce ''
-          ${exporterArgs}
-        '';
-        RestrictAddressFamilies = [
-          "AF_INET"
-          "AF_INET6"
-          "AF_UNIX"
+      services.fail2ban.jails.caddy-access.settings = {
+        enabled = true;
+        filter = "caddy-access";
+        logpath = "/var/log/caddy/access-*.log";
+        backend = "auto";
+        port = "http,https";
+        findtime = 30;
+        maxretry = 5;
+        bantime = 600;
+      }
+      // lib.optionalAttrs cfg.cloudflare.enable {
+        action = lib.concatStringsSep "\n" [
+          "%(action_)s"
+          "  cloudflare-edge-ban"
         ];
       };
-    };
 
-    services.prometheus.scrapeConfigs = optionals prometheusEnabled [
-      {
-        job_name = "fail2ban";
-        static_configs = [
-          {
-            targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.fail2ban.port}" ];
-          }
-        ];
-      }
-    ];
-  };
+      services.prometheus.exporters.fail2ban = {
+        enable = true;
+        host = "127.0.0.1";
+        port = ports.exporters.fail2ban;
+      };
+
+      systemd.services.prometheus-fail2ban-exporter = mkIf exporterCfg.enable {
+        requires = mkIf config.services.fail2ban.enable [ "prometheus-fail2ban-exporter-setup.service" ];
+        serviceConfig = {
+          DynamicUser = false;
+          ExecStart = lib.mkForce ''
+            ${exporterArgs}
+          '';
+          RestrictAddressFamilies = [
+            "AF_INET"
+            "AF_INET6"
+            "AF_UNIX"
+          ];
+        };
+      };
+    }
+    (mkIf cfg.cloudflare.enable {
+      sops.secrets.cloudflare-fail2ban-token = {
+        sopsFile = lib.snowfall.fs.get-file "secrets/cloudflare/fail2ban.yaml";
+        format = "yaml";
+        key = "token";
+      };
+      sops.secrets.cloudflare-fail2ban-zone-id = {
+        sopsFile = lib.snowfall.fs.get-file "secrets/cloudflare/fail2ban.yaml";
+        format = "yaml";
+        key = "zone_id";
+      };
+
+      environment.etc."fail2ban/action.d/cloudflare-edge-ban.conf".text = ''
+        [Definition]
+        actionstart =
+        actionstop =
+        actioncheck =
+        actionban = ${getExe pkgs.curl} -s -X POST "<_cf_api_url>" <_cf_api_prms> --data '{"mode":"<cfmode>","configuration":{"target":"<cftarget>","value":"<ip>"},"notes":"<notes>"}'
+        actionunban = id=$(${getExe pkgs.curl} -s -G -X GET "<_cf_api_url>" --data-urlencode "mode=<cfmode>" --data-urlencode "notes=<notes>" --data-urlencode "configuration.target=<cftarget>" --data-urlencode "configuration.value=<ip>" <_cf_api_prms> | ${getExe pkgs.gawk} -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/"id"/){print $(i+1)}}}' | tr -d ' "' | head -n 1)
+        	if [ -z "$id" ]; then echo "<name>: id for <ip> cannot be found using target <cftarget>"; exit 0; fi; \
+        	${getExe pkgs.curl} -s -X DELETE "<_cf_api_url>/$id" <_cf_api_prms> --data '{"cascade": "none"}'
+        _cf_api_url = https://api.cloudflare.com/client/v4/zones/$(cat <zoneidfile>)/firewall/access_rules/rules
+        _cf_api_prms = -H "Authorization: Bearer $(cat <tokenfile>)" -H "Content-Type: application/json"
+
+        [Init]
+        cftarget = ip
+        cfmode = block
+        notes = Fail2Ban <name>
+        tokenfile = ${config.sops.secrets.cloudflare-fail2ban-token.path}
+        zoneidfile = ${config.sops.secrets.cloudflare-fail2ban-zone-id.path}
+
+        [Init?family=inet6]
+        cftarget = ip6
+      '';
+    })
+    (mkIf prometheusEnabled {
+      services.prometheus.scrapeConfigs = [
+        {
+          job_name = "fail2ban";
+          static_configs = [
+            {
+              targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.fail2ban.port}" ];
+            }
+          ];
+        }
+      ];
+    })
+  ];
 }
