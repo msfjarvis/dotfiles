7182217d3bf1844fa634ffa148c2b0c940b9449e
Harsh Shandilya <me@msfjarvis.dev>
2026-07-01 00:20:59 +0530

    bump rust

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -42,7 +42,7 @@
         system:
         (import fenix { pkgs = nixpkgsFor.${system}; }).fromToolchainFile {
           file = ./rust-toolchain.toml;
-          sha256 = "sha256-mvUGEOHYJpn3ikC5hckneuGixaC+yGrkMM/liDIDgoU=";
+          sha256 = "sha256-h+t2xTBz5yt2YIO+1VMIIGlCU7gyp2LYOFvaV1nwOXU=";
         }
       );
       craneLib = eachSystem (
