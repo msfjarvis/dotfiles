e29004287321ddd51c9bb06eb38b2a7e4f8dace6
Harsh Shandilya <me@msfjarvis.dev>
2026-07-02 08:11:18 +0530

    revert rsync override
    fails to build with new nixpkgs

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -5,15 +5,5 @@ _: prev: {
   megatools = prev.megatools.overrideAttrs (_: {
     patches = [ ./megatools.patch ];
   });
-  # rsync 3.4.3 is going through an infinite regression simulator thanks to Claude
-  rsync = prev.rsync.overrideAttrs (_: rec {
-    version = "3.4.1";
-
-    src = prev.fetchurl {
-      # signed with key 9FEF 112D CE19 A0DC 7E88  2CB8 1BB2 4997 A853 5F6F
-      url = "mirror://samba/rsync/src/rsync-${version}.tar.gz";
-      hash = "sha256-KSS8s6Hti1UfwQH3QLnw/gogKxFQJ2R89phQ1l/YjFI=";
-    };
-  });
   qbittorrent = prev.qbittorrent.override { guiSupport = false; };
 }
