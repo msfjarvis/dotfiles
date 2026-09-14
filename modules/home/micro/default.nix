b226f3a4695f4248450dd4528217c324817b2b3f
Harsh Shandilya <me@msfjarvis.dev>
2026-09-14 17:20:45 +0530

    disable EOF newline for micro

--- modules/home/micro/default.nix
+++ modules/home/micro/default.nix
@@ -4,6 +4,8 @@
     enable = true;
     settings = {
       colorscheme = "custom";
+      # Too annoying with pi's settings.json and I don't care as much anymore
+      eofnewline = false;
       mkparents = true;
       softwrap = true;
       wordwrap = true;
